ad8d16941f0d0222fb36df567068305dc28a5917
AlphaEmperorOmega <=>
2025-02-18 22:44:47 +0100

    [ADD]: Unit tests file

---
+++ tests.py
@@ -0,0 +1,9 @@
+import unittest
+
+from Emperor.tests.parameter_generators.vector_choice import *
+from Emperor.tests.parameter_generators.matrix_choice import *
+from Emperor.tests.parameter_generators.generator_choice import *
+# from Emperor.tests.library.torch import *
+
+if __name__ == "__main__":
+    unittest.main()
