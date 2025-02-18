1bf91df9f2b04545e6676b208da694b5e064bf31
AlphaEmperorOmega <=>
2025-02-18 20:16:54 +0100

    [ADD]: decorator classes and functions
    - timer function used to track the amout of time a function takes to
    process an `inputBatch` for example

---
+++ Emperor/base/decorators.py
@@ -0,0 +1,11 @@
+import time
+
+# Define a timer decorator
+def timer(func):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        end = time.time()
+        print(f"{func.__name__} executed in {end - start:.4f} seconds")
+        return result
+    return wrapper
