172c13da0e41ebda1a476295750d60d8de8c1ebf
AlphaEmperorOmega <alpha.emperor.omega@gmail.com>
2025-04-22 18:22:10 +0200

    [UPD]: ParameterGeneratorLayer refactoring work in progress

--- Emperor/components/layer.py
+++ Emperor/components/layer.py
@@ -1,40 +1,55 @@
-from ..library.choice import Library as L
+import torch
+from torch import Tensor
+
 from ..base.utils import Module
 from ..base.decorators import timer
-
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Optional
 
 if TYPE_CHECKING:
-    from Emperor.config import ParameterGeneratorConfig
+    from Emperor.config import Configuration, ParameterGeneratorOptions
 
 
 class ParameterGeneratorLayer(Module):
-    def __init__(self, cfg: "ParameterGeneratorConfig"):
+    def __init__(
+        self,
+        cfg: Optional["Configuration"] = None,
+        track_time_flag: Optional[bool] = None,
+        parameter_geneartor_type: Optional[ParameterGeneratorOptions] = None,
+    ):
         super().__init__()
+        self.cfg: "ParameterGeneratorConfig" = self._get_config(
+            cfg, "parameter_geneartor_config"
+        )
+        self.track_time_flag = self._resolve(track_time_flag, self.cfg.track_time_flag)
+        self.parameter_geneartor_type = self._resolve(
+            parameter_geneartor_type, self.cfg.parameter_geneartor_type
+        )
+        self._init_parameter_generator(cfg)
 
-        self.layerTimeFlag = False
-        self.parameterGenerator = cfg.parameterGeneartorType.create(cfg)
-        self.gatherFrequency = cfg.gatherFrequencyFlag
+    def _init_parameter_generator(self, cfg: Optional["Configuration"]) -> None:
+        assert self.parameter_geneartor_type is not None, (
+            f"Expected `parameter_generator` to be `ParameterGeneratorOptions`, received {type(self.parameter_geneartor_type)}"
+        )
+        self.parameter_generator = self.parameter_geneartor_type.create(cfg)
 
-    def forward(self, inputBatch):
-        if self.layerTimeFlag:
-            return self.forwardWithTimer(inputBatch)
-        return self.forwardDefault(inputBatch)
+    def forward(self, input_batch: Tensor) -> Tensor:
+        # TODO: find out the shape of `input_batch` tensor
+        if self.track_time_flag:
+            return self.track_forward_time(input_batch)
+        return self.compute_layer_output(input_batch)
 
     @timer
-    def forwardWithTimer(self, inputBatch):
-        return self.forwardDefault(inputBatch)
+    def track_forward_time(self, input_batch: Tensor) -> Tensor:
+        return self.compute_layer_output(input_batch)
 
-    def forwardDefault(self, inputBatch):
-        generatedWeights, generatedBiases, probabilities = self.parameterGenerator(
-            inputBatch
-        )
+    def compute_layer_output(self, input_batch: Tensor) -> Tensor:
+        generated_weights, generated_biases = self.parameter_generator(input_batch)
 
-        output = L.tensorProduct(inputBatch.unsqueeze(1), generatedWeights).squeeze(1)
-        output = (output + generatedBiases) if generatedBiases is not None else output
+        input_batch = input_batch.unsqueeze(1)
+        output = torch.matmul(input_batch, generated_weights)
+        output = output.squeeze(1)
 
-        # Scale each output vector by the mean of probabilities of it's selected weights
-        # if probabilities is not None:
-        #     output = output * probabilities
+        if generated_biases is not None:
+            output = output + generated_biases
 
         return output
