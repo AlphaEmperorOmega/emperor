04d6b06c7efcc077129eeab65d56b68fc6ae453d
AlphaEmperorOmega <alpha.emperor.omega@gmail.com>
2025-05-27 20:53:20 +0200

    [UPD]: Updated the unit test wrapper

--- tests.py
+++ tests.py
@@ -5,7 +5,12 @@ import unittest
 # from tests.generators.matrix_choice import *
 # from tests.parameter_generators.generator_choice import *
 # from mperor.tests.library.torch import *
-from tests.attention.attention import *
+# from tests.attention.attention import *
+
+# from tests.test_routers import *
+# from tests.test_samplers import *
+from tests.test_mixtures import *
+
 
 if __name__ == "__main__":
     unittest.main()
