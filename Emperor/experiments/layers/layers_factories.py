b5f0018165d8147327383f90e49e65afdab6ed8b
AlphaEmperorOmega <alpha.emperor.omega@gmail.com>
2025-06-11 17:18:05 +0200

    [ADD]: Added factories for ParameterGenerator experiments #62

---
+++ Emperor/experiments/layers/layers_factories.py
@@ -0,0 +1,114 @@
+from enum import Enum
+
+from Emperor.components.parameter_generators.layers import (
+    DefaultLinearLayer,
+    GeneratorParameterLayer,
+    MatrixParameterLayer,
+    ParameterLayerBase,
+    VectorParameterLayer,
+)
+
+from typing import TYPE_CHECKING
+
+from Emperor.experiments.layers.layers_models import (
+    MultiLayerClassifierModel,
+    SingleLayerClassifierModel,
+)
+from Emperor.experiments.layers.layers_presets import (
+    FashionMNISTModelTrainer,
+    ParameterLayerPresetFactory,
+)
+from Emperor.model_config_presets import ModelConfigPresets
+
+if TYPE_CHECKING:
+    from Emperor.config import ModelConfig
+    from Emperor.experiments.layers.layers_models import ClassifierExperiment
+
+
+class ModelFactory(Enum):
+    SINGLE_LAYER = SingleLayerClassifierModel
+    MULTI_LAYER = MultiLayerClassifierModel
+
+    def create(
+        self,
+        model: "ParameterLayerBase",
+        learning_rate: float = 0.1,
+    ) -> "ClassifierExperiment":
+        return self.value(model, learning_rate)
+
+
+class ParameterLayerOptions(Enum):
+    DEFAULT = DefaultLinearLayer
+    VECTOR = VectorParameterLayer
+    MATRIX = MatrixParameterLayer
+    GENERATOR = GeneratorParameterLayer
+
+    def create(
+        self,
+        cfg: "ModelConfig",
+    ) -> ParameterLayerBase:
+        return self.value(cfg)
+
+
+class ParameterLayerPresetOptions(Enum):
+    SPARSE = "create_sparse_layer"
+    TOPK = "create_topk_layer"
+    FULL_MIXTURE = "create_full_mixture_layer"
+    RANDOM_TOPK = "create_random_topk_layer"
+    SPARSE_THRESHOLD = "create_sparse_threshold_layer"
+    TOPK_THRESHOLD = "create_topk_threshold_layer"
+    FULL_MIXTURE_THRESHOLD = "create_full_mixture_threshold_layer"
+    SPARSE_NOISY_TOPK = "create_sparse_noisy_topk_layer"
+    TOPK_NOISY_TOPK = "create_topk_noisy_topk_layer"
+    FULL_MIXTURE_NOISY_TOPK = "create_full_mixture_noisy_topk_layer"
+
+    def train_single_layer_fashion_minist_dataset(
+        self,
+        layer_type: "ParameterLayerOptions",
+    ) -> None:
+        cfg = ModelConfigPresets.fashion_minist_model_config()
+        parameter_layer_preset = ParameterLayerPresetFactory(layer_type, cfg)
+        parameter_layer_preset = getattr(parameter_layer_preset, self.value)()
+        model = SingleLayerClassifierModel(
+            parameter_layer_preset,
+            learning_rate=0.0001,
+        )
+        dataset = FashionMNISTModelTrainer(
+            model,
+            cfg,
+            test_dataset_flag=False,
+        )
+        dataset.train()
+
+    def train_multi_layer_fashion_minist_dataset(
+        self,
+        layer_preset: "ParameterLayerOptions",
+    ) -> None:
+        cfg = ModelConfigPresets.fashion_minist_model_config()
+
+        def create_hidden_layer_model(
+            cfg: "ModelConfig",
+            layer_shape: list[int],
+            layer_type: "ParameterLayerBase | None" = None,
+        ):
+            input_dim, output_dim = layer_shape
+            parameter_layer_preset = ParameterLayerPresetFactory(layer_preset, cfg)
+            parameter_layer_preset.set_layer_input_dim(input_dim)
+            parameter_layer_preset.set_layer_output_dim(output_dim)
+
+            layer_type_method = self.value if layer_type is None else layer_type
+
+            return getattr(parameter_layer_preset, self.value)()
+
+        model = MultiLayerClassifierModel(
+            cfg=cfg,
+            layer_preset=layer_preset,
+            hidden_layer_callback=create_hidden_layer_model,
+            num_hidden_layers=2,
+        )
+        dataset = FashionMNISTModelTrainer(
+            model,
+            cfg,
+            test_dataset_flag=False,
+        )
+        dataset.train()
