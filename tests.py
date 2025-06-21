fd9e1a2e31500e0b7765c773d2afdffedac37d67
AlphaEmperorOmega <alpha.emperor.omega@gmail.com>
2025-06-21 23:58:02 +0200

    [UPD]: Added new tests to the test file

--- tests.py
+++ tests.py
@@ -1,15 +1,17 @@
 import unittest
 
-# from tests.generators.vector_choice import *
-# from tests.generators.vector_choice import *
-# from tests.generators.matrix_choice import *
 # from tests.parameter_generators.generator_choice import *
 # from mperor.tests.library.torch import *
 # from tests.attention.attention import *
 
-# from tests.test_routers import *
-# from tests.test_samplers import *
+from tests.test_routers import *
+
+from tests.test_samplers import *
 from tests.test_mixtures import *
+from tests.test_layers import *
+from tests.test_behaviours import *
+from tests.test_linears import *
+from tests.test_losses import *
 
 
 if __name__ == "__main__":
