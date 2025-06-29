e8f817d87bbb88c04465ed22fabcd0640eead4e2
AlphaEmperorOmega <alpha.emperor.omega@gmail.com>
2025-06-30 00:06:26 +0200

    [UPD]: Split the LinearLayer and DynamicLinearLayer in two unit test classes

--- tests/test_linears.py
+++ tests/test_linears.py
@@ -104,7 +104,7 @@ class TestLinearLayers(unittest.TestCase):
                 bias_parameters_flag=MIXTURE_BIAS_PARAMETERS_FLAG,
                 weighted_parameters_flag=MIXTURE_WEIGHTED_PARAMETERS_FLAG,
                 num_experts=MIXTURE_ROUTER_OUTPUT_DIM,
-                cross_diagonal_flag=MIXTURE_CROSS_DIAGONAL_FLAG,
+                dynamic_diagonal_params_flag=MIXTURE_CROSS_DIAGONAL_FLAG,
             ),
             parameter_generator_model_config=ParameterLayerConfig(
                 bias_parameters_flag=PARAMETER_GENERATOR_BIAS_PARAMETER_FLAG,
@@ -154,11 +154,114 @@ class TestLinearLayers(unittest.TestCase):
         output = m.forward(input_batch)
         self.assertEqual(list(output.shape), [batch_size, m.output_dim])
 
-    def test__cross_diagonal_linear_layer__init_with_main_config(self):
+
+class TestDynamicDiagonalLinearLayer(unittest.TestCase):
+    def setUp(self):
+        # MODEL WISE CONFI
+        BATCH_SIZE = 2
+        INPUT_DIM = 4
+        HIDDEN_DIM = 5
+        OUTPUT_DIM = 6
+        GATHER_FREQUENCY_FLAG = False
+
+        # AUXILIARY LOSSES OPITONS
+        COEFFICIENT_OF_VARIATION_LOSS_WEIGHT: float = 0.0
+        SWITCH_LOSS_WEIGHT: float = 0.0
+        ZERO_CENTERED_LOSS_WEIGHT: float = 0.0
+        MUTUAL_INFORMATION_LOSS_WEIGHT: float = 0.0
+
+        # PARAMETER GENRETOR ROUTER OPITONS
+        ROUTER_INPUT_DIM = HIDDEN_DIM
+        ROUTER_HIDDEN_DIM = 8
+        ROUTER_OUTPUT_DIM = 9
+        ROUTER_NOISY_TOPK_FLAG = False
+        ROUTER_ACTIVATION_FUNCTION = nn.ReLU()
+        ROUTER_NUM_LAYERNUM_LAYERSS = 5
+        ROUTER_DYNAMIC_LINEAR_MODEL_FLAG = False
+
+        # PARAMETER GENRETOR SAMPLER OPITONS
+        SAMPLER_TOP_K = 3
+        SAMPLER_THRESHOLD = 0.1
+        SAMPLER_DYNAMIC_TOPK_THRESHOLD = (1 / ROUTER_OUTPUT_DIM) * 1e-6
+        SAMPLER_NUM_TOPK_SAMPLES = 0
+        SAMPLER_NORMALIZE_PROBABILITIES_FLAG = False
+        SAMPLER_NOISY_TOPK_FLAG = ROUTER_NOISY_TOPK_FLAG
+        SAMPLER_ROUTER_OUTPUT_DIM = ROUTER_OUTPUT_DIM
+        SAMPLER_COEFFICIENT_OF_VARIATION_WEIGHT = 0.0
+        SAMPLER_SWITCH_WEIGHT = 0.0
+        SAMPLER_ZERO_CENTRED_WEIGHT = 0.0
+        SAMPLER_MUTUAL_INFORMATION_WEIGHT = 0.0
+
+        # PARAMETER GENRETOR MIXTURE OPITONS
+        MIXTURE_INPUT_DIM = ROUTER_INPUT_DIM
+        MIXTURE_OUTPUT_DIM = ROUTER_OUTPUT_DIM
+        MIXTURE_DEPTH_DIM = ROUTER_OUTPUT_DIM
+        MIXTURE_TOP_K = SAMPLER_TOP_K
+        MIXTURE_WEIGHTED_PARAMETERS_FLAG = False
+        MIXTURE_BIAS_PARAMETERS_FLAG = False
+        MIXTURE_ROUTER_OUTPUT_DIM = ROUTER_OUTPUT_DIM
+        MIXTURE_CROSS_DIAGONAL_FLAG = False
+
+        # PARAMETER GENERATOR OPTIONS
+        PARAMETER_GENERATOR_BIAS_PARAMETER_FLAG = MIXTURE_WEIGHTED_PARAMETERS_FLAG
+
+        self.cfg = ModelConfig(
+            batch_size=BATCH_SIZE,
+            input_dim=INPUT_DIM,
+            hidden_dim=HIDDEN_DIM,
+            output_dim=OUTPUT_DIM,
+            gather_frequency_flag=GATHER_FREQUENCY_FLAG,
+            router_model_config=RouterConfig(
+                input_dim=ROUTER_INPUT_DIM,
+                hidden_dim=ROUTER_HIDDEN_DIM,
+                output_dim=ROUTER_OUTPUT_DIM,
+                noisy_topk_flag=ROUTER_NOISY_TOPK_FLAG,
+                activation=ROUTER_ACTIVATION_FUNCTION,
+                num_layers=ROUTER_NUM_LAYERNUM_LAYERSS,
+                diagonal_linear_model_flag=ROUTER_DYNAMIC_LINEAR_MODEL_FLAG,
+            ),
+            sampler_model_config=SamplerConfig(
+                top_k=SAMPLER_TOP_K,
+                threshold=SAMPLER_THRESHOLD,
+                num_topk_samples=SAMPLER_NUM_TOPK_SAMPLES,
+                normalize_probabilities_flag=SAMPLER_NORMALIZE_PROBABILITIES_FLAG,
+                noisy_topk_flag=SAMPLER_NOISY_TOPK_FLAG,
+                num_experts=SAMPLER_ROUTER_OUTPUT_DIM,
+                coefficient_of_variation_weight=SAMPLER_COEFFICIENT_OF_VARIATION_WEIGHT,
+                switch_weight=SAMPLER_SWITCH_WEIGHT,
+                zero_centred_weight=SAMPLER_ZERO_CENTRED_WEIGHT,
+                mutual_information_weight=SAMPLER_MUTUAL_INFORMATION_WEIGHT,
+            ),
+            mixture_model_config=MixtureConfig(
+                input_dim=MIXTURE_INPUT_DIM,
+                output_dim=MIXTURE_OUTPUT_DIM,
+                depth_dim=MIXTURE_DEPTH_DIM,
+                top_k=MIXTURE_TOP_K,
+                bias_parameters_flag=MIXTURE_BIAS_PARAMETERS_FLAG,
+                weighted_parameters_flag=MIXTURE_WEIGHTED_PARAMETERS_FLAG,
+                num_experts=MIXTURE_ROUTER_OUTPUT_DIM,
+                dynamic_diagonal_params_flag=MIXTURE_CROSS_DIAGONAL_FLAG,
+            ),
+            parameter_generator_model_config=ParameterLayerConfig(
+                bias_parameters_flag=PARAMETER_GENERATOR_BIAS_PARAMETER_FLAG,
+            ),
+            linear_layer_model_config=LinearLayerConfig(
+                input_dim=INPUT_DIM,
+                output_dim=OUTPUT_DIM,
+                bias_flag=True,
+                anti_diagonal_flag=True,
+            ),
+        )
+
+        self.parameter_generator_cfg = self.cfg.mixture_model_config
+
+    def test__init_with_main_config(self):
         m = DynamicDiagonalLinearLayer(self.cfg)
-        self.assertIsInstance(m.diagonal_behaviour, DynamicDiagonalParametersBehaviour)
+        self.assertIsInstance(
+            m.dynamic_diagonal_params_model, DynamicDiagonalParametersBehaviour
+        )
 
-    def test__cross_diagonal_linear_layer__forward__anti_diagonal_flag__False(self):
+    def test__forward__anti_diagonal_flag__False(self):
         c = copy.deepcopy(self.cfg)
         overrides = LinearLayerConfig(anti_diagonal_flag=False)
         m = DynamicDiagonalLinearLayer(c, overrides)
@@ -170,14 +273,87 @@ class TestLinearLayers(unittest.TestCase):
         output = m.forward(input_batch)
         self.assertEqual(list(output.shape), [batch_size, m.output_dim])
 
-    def test__cross_diagonal_linear_layer__forward__anti_diagonal_flag__True(self):
+    def test__forward__anti_diagonal_flag__True(self):
         c = copy.deepcopy(self.cfg)
-        overrides = LinearLayerConfig(anti_diagonal_flag=False)
+        overrides = LinearLayerConfig(
+            anti_diagonal_flag=True,
+        )
         m = DynamicDiagonalLinearLayer(c, overrides)
         batch_size = 2
         input_batch_shape = (batch_size, self.cfg.input_dim)
         input_batch = (
             torch.arange(prod(input_batch_shape)).reshape(input_batch_shape).float()
         )
+
         output = m.forward(input_batch)
         self.assertEqual(list(output.shape), [batch_size, m.output_dim])
+
+    def test__forward__dynamic_bias_flag__True(self):
+        c = copy.deepcopy(self.cfg)
+        overrides = LinearLayerConfig(
+            dynamic_bias_flag=True,
+        )
+        m = DynamicDiagonalLinearLayer(c, overrides)
+        batch_size = 2
+        input_batch_shape = (batch_size, self.cfg.input_dim)
+        input_batch = (
+            torch.arange(prod(input_batch_shape)).reshape(input_batch_shape).float()
+        )
+
+        output = m.forward(input_batch)
+        self.assertEqual(list(output.shape), [batch_size, m.output_dim])
+
+    def test__forward__all_flags_true(self):
+        c = copy.deepcopy(self.cfg)
+        overrides = LinearLayerConfig(
+            dynamic_bias_flag=True,
+            bias_flag=True,
+            anti_diagonal_flag=True,
+        )
+        m = DynamicDiagonalLinearLayer(c, overrides)
+        batch_size = 2
+        input_batch_shape = (batch_size, self.cfg.input_dim)
+        input_batch = (
+            torch.arange(prod(input_batch_shape)).reshape(input_batch_shape).float()
+        )
+
+        output = m.forward(input_batch)
+        self.assertEqual(list(output.shape), [batch_size, m.output_dim])
+
+    def test__compute_linear_transformation(self):
+        c = copy.deepcopy(self.cfg)
+        overrides = LinearLayerConfig(anti_diagonal_flag=False)
+        m = DynamicDiagonalLinearLayer(c, overrides)
+        batch_size = 2
+        shape = (batch_size, self.cfg.input_dim)
+        logits = torch.randn(shape)
+        shape = (batch_size, self.cfg.input_dim, self.cfg.output_dim)
+        dynamic_weight_params = torch.randn(shape)
+        output = m._DynamicDiagonalLinearLayer__compute_linear_transformation(
+            logits, dynamic_weight_params
+        )
+        for i in range(batch_size):
+            expected_vector = torch.matmul(logits[i], dynamic_weight_params[i])
+            self.assertTrue(torch.allclose(output[i], expected_vector))
+
+        self.assertEqual(list(output.shape), [batch_size, m.output_dim])
+
+    def test__add_bias_parameters__bias_flag__True(self):
+        c = copy.deepcopy(self.cfg)
+        overrides = LinearLayerConfig(
+            anti_diagonal_flag=False,
+            bias_flag=True,
+        )
+        m = DynamicDiagonalLinearLayer(c, overrides)
+        batch_size = 2
+        shape = (batch_size, self.cfg.output_dim)
+        linear_transform = torch.randn(shape)
+        shape = (1, self.cfg.output_dim)
+        bias_params = torch.randn(shape)
+        output = m._DynamicDiagonalLinearLayer__add_bias_parameters(
+            linear_transform, bias_params
+        )
+
+        expected_output = linear_transform + bias_params
+        self.assertEqual(list(output.shape), [batch_size, m.output_dim])
+        self.assertTrue(torch.allclose(output, expected_output))
