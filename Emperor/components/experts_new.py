787bc768e1bad83b8fa177b647237564fe619563
AlphaEmperorOmega <alpha.emperor.omega@gmail.com>
2025-07-16 08:39:35 +0200

    [UPD]: Temp file to refactor mixture of experts

---
+++ Emperor/components/experts_new.py
@@ -0,0 +1,141 @@
+from .layer import ParameterGeneratorLayer
+from Emperor.library.choice import Library as L
+from Emperor.base.utils import Module
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from Emperor.config import ModelConfig
+
+
+class ParallelExperts(Module):
+    def __init__(self, cfg: "ModelConfig"):
+        super().__init__()
+
+        self.cfg = cfg
+        self.numExperts = cfg.numExperts
+        self.experts = self._createExperts()
+
+    def _createExperts(self):
+        experts = []
+        for _ in range(self.numExperts):
+            layer = ParameterGeneratorLayer(self.cfg)
+            experts.append(layer)
+
+        return L.ModuleList(experts)
+
+    def forward(self, expertOrderedInput, expertFrequency):
+        expertInputs = self._splitInputExperts(expertOrderedInput, expertFrequency)
+        expertOutputs = []
+        for expertIndex in range(self.numExperts):
+            input = expertInputs[expertIndex]
+            output = self.experts[expertIndex](input)
+            expertOutputs.append(output)
+
+        return L.cat(expertOutputs, dim=0)
+
+    def _splitInputExperts(self, expertOrderedInput, expertFrequency):
+        expertFrequencyList = expertFrequency.tolist()
+        return expertOrderedInput.split(expertFrequencyList)
+
+    def _computeGating(self, topKProbabilities, topKIndexes):
+        expert_sorted_indices = self.expert_sorted_indices(
+            topKProbabilities, topKIndexes
+        )
+        expert_sroted_probabilities = self.expert_sorted_probabilities(
+            topKProbabilities, topKIndexes
+        )
+        scattered_probabilities = self.scattered_probabiliites(
+            topKProbabilities, topKIndexes
+        )
+        expert_frequency = self.batch_frequency(topKProbabilities, topKIndexes)
+        expert_sorted_nonzero_probability_indices = (
+            self.expert_sorted_nonzero_probability_indexes(
+                topKProbabilities, topKIndexes
+            )
+        )
+
+        return (
+            expert_sorted_indices,
+            expert_sroted_probabilities,
+            scattered_probabilities,
+            expert_frequency,
+            expert_sorted_nonzero_probability_indices,
+        )
+
+    def expert_sorted_indices(self, topKProbabilities, topKIndexes):
+        _, _, nonzeroProbabilityIndexes, _expertSortedNonzeroTopKIndexes = (
+            self.required_inputs(topKProbabilities, topKIndexes)
+        )
+
+        expertSortedNonzeroProbabilityIndexes = nonzeroProbabilityIndexes[
+            _expertSortedNonzeroTopKIndexes
+        ]
+
+        expertSortedTopKBatchIndexes = expertSortedNonzeroProbabilityIndexes.div(
+            self.cfg.topK, rounding_mode="trunc"
+        )
+
+        return expertSortedTopKBatchIndexes
+
+    def expert_sorted_probabilities(self, topKProbabilities, topKIndexes):
+        (
+            _,
+            topKProbabilitiesFlattened,
+            nonzeroProbabilityIndexes,
+            _expertSortedNonzeroTopKIndexes,
+        ) = self.required_inputs(topKProbabilities, topKIndexes)
+
+        expertSortedNonzeroProbabilityIndexes = nonzeroProbabilityIndexes[
+            _expertSortedNonzeroTopKIndexes
+        ]
+        expertSortedTopKBatchProbabilities = topKProbabilitiesFlattened[
+            expertSortedNonzeroProbabilityIndexes
+        ]
+
+        return expertSortedTopKBatchProbabilities
+
+    def scattered_probabiliites(self, topKProbabilities, topKIndexes):
+        topKProbabilitiesScatteredPlaceholder = L.zeros(
+            [topKProbabilities.shape[0], self.cfg.numExperts], device=L.Device
+        )
+        topKProbabilitiesScattered = topKProbabilitiesScatteredPlaceholder.scatter(
+            1, topKIndexes, topKProbabilities
+        )
+        return topKProbabilitiesScattered
+
+    def batch_frequency(self, topKProbabilities, topKIndexes):
+        batchExpertFrequency = (
+            self.scattered_probabiliites(topKProbabilities, topKIndexes) > 0
+        )
+        batchExpertFrequency = batchExpertFrequency.long()
+        batchExpertFrequency = batchExpertFrequency.sum(dim=0)
+
+        return batchExpertFrequency
+
+    def expert_sorted_nonzero_probability_indexes(self, topKProbabilities, topKIndexes):
+        _, _, nonzeroProbabilityIndexes, _expertSortedNonzeroTopKIndexes = (
+            self.required_inputs(topKProbabilities, topKIndexes)
+        )
+
+        expertSortedNonzeroProbabilityIndexes = nonzeroProbabilityIndexes[
+            _expertSortedNonzeroTopKIndexes
+        ]
+        return expertSortedNonzeroProbabilityIndexes
+
+    def required_inputs(self, topKProbabilities, topKIndexes):
+        topKIndexesFlattened = topKIndexes.flatten()
+        topKProbabilitiesFlattened = topKProbabilities.flatten()
+
+        nonzeroProbabilityIndexes = topKProbabilitiesFlattened.nonzero()
+        nonzeroProbabilityIndexes = nonzeroProbabilityIndexes.squeeze(-1)
+
+        nonzeroTopKIndexes = topKIndexesFlattened[nonzeroProbabilityIndexes]
+        _, _expertSortedNonzeroTopKIndexes = nonzeroTopKIndexes.sort(0)
+
+        return (
+            topKIndexesFlattened,
+            topKProbabilitiesFlattened,
+            nonzeroProbabilityIndexes,
+            _expertSortedNonzeroTopKIndexes,
+        )
