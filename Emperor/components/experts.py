3a4c523b91c96539842b22f7c1fa228d4559e0aa
AlphaEmperorOmega <alpha.emperor.omega@gmail.com>
2025-07-17 21:14:58 +0200

    [UPD]: Mixture of experts refactoring model - progress #75
    - Renamed ParallelExperts > ExpertLayer
    - Removed old indice shufling function for a simplified version
    - ExpertLayer now uses a simplified function that samples the input
    indices for each expert

--- Emperor/components/experts.py
+++ Emperor/components/experts.py
@@ -80,94 +80,53 @@ class MixtureOfExperts(Module):
         self.utils = ExpertSelectorUtils()
         self.router = RouterModel(cfg)
         self.sampler = SamplerModel(cfg)
-        self.input_experts = ParallelExperts(self.input_experts_config)
+        self.input_experts = ExpertsLayer(self.input_experts_config)
         # Output experts should be raw
-        self.output_experts = ParallelExperts(self.output_experts_config)
+        self.output_experts = ExpertsLayer(self.output_experts_config)
+        self.input_batch_shape = None
 
     def forward(
         self,
         input_batch: Tensor,
         skip_mask: Tensor | None = None,
-    ):
-        self._input_batch_shape = input_batch.size()
-
-        input_batch_matrix, skip_mask = self.__reshape_inputs(input_batch, skip_mask)
-
-        logits = self.router.compute_logit_scores(input_batch)
+    ) -> tuple[Tensor, Tensor | None]:
+        input_batch_matrix, skip_mask = self.__prepare_inputs(input_batch, skip_mask)
+        logits = self.router.compute_logit_scores(input_batch_matrix)
         probabilities, indices, skip_mask, loss = (
             self.sampler.sample_probabilities_and_indices(logits, skip_mask)
         )
+        experts_output = self.__compute_projections(input_batch, indices)
 
-        (
-            expert_sorted_batch_inputs,
-            expert_sorted_top_k_batch_indexes,
-            expert_sorted_top_k_batch_probabilities,
-            batch_expert_frequency,
-            expert_sorted_nonzero_probability_indexes,
-        ) = self._prepare_inputs(input_batch_matrix, probabilities, indices)
-
-        feed_forward_output = self._compute_projections(
-            expert_sorted_batch_inputs, batch_expert_frequency
-        )
-
-        expert_mixture_output = self._compute_mixture(
-            feed_forward_output,
-            expert_sorted_top_k_batch_indexes,
-            expert_sorted_top_k_batch_probabilities,
+        expert_mixture_output = self.__compute_expert_mixture(
+            experts_output,
+            indices,
+            probabilities,
         )
 
         return expert_mixture_output
 
-    def _prepare_inputs(
-        self,
-        input_batch: Tensor,
-        probabilities: Tensor,
-        indices: Tensor,
-    ):
-        (
-            expert_sorted_indices,
-            expert_sroted_probabilities,
-            scattered_probabilities,
-            expert_frequency,
-            expert_sorted_nonzero_probability_indices,
-        ) = self.utils.compute_gating(probabilities, indices)
-
-        expert_sorted_input_batch = input_batch[expert_sorted_indices]
-
-        return (
-            expert_sorted_input_batch,
-            expert_sroted_probabilities,
-            scattered_probabilities,
-            expert_frequency,
-            expert_sorted_nonzero_probability_indices,
-        )
-
-    def __reshape_inputs(
+    def __prepare_inputs(
         self,
         input_batch: Tensor,
         skip_mask: Tensor | None = None,
     ) -> tuple[Tensor, Tensor | None]:
+        self.input_batch_shape = input_batch.size()
         input_batch = input_batch.reshape(-1, self.inputDim)
         if skip_mask is not None:
             skip_mask = skip_mask.view(-1, 1)
         return input_batch, skip_mask
 
-    def _compute_projections(
+    def __compute_projections(
         self,
-        expert_sorted_inputs: Tensor,
-        frequency: Tensor,
-    ):
-        expanded_projection = self.input_experts(expert_sorted_inputs, frequency)
-        # hidden_representation = self.activationFunction(expanded_projection)
-        # if self.dropoutModule:
-        #     hidden_representation = self.dropoutModule(hidden_representation)
-        # if self.hiddenLayerNormModule:
-        #     hidden_representation = self.hiddenLayerNormModule(hidden_representation)
-        reduction_projection = self.output_experts(expanded_projection, frequency)
+        input_batch: Tensor,
+        indices: Tensor | None = None,
+    ) -> Tensor:
+        expanded_projection = self.input_experts(input_batch, indices)
+        reduction_projection = self.output_experts(expanded_projection, indices)
 
         return reduction_projection
 
-    def _compute_expert_mixture(
+    def __compute_expert_mixture(
         self,
         experts_output,
         expert_sorted_indices,
@@ -198,99 +157,6 @@ class MixtureOfExperts(Module):
         return output.view(output_shape)
 
 
-class ExpertSelectorUtils:
-    def compute_gating(self, probabilities, indexes):
-        expert_sroted_probabilities, expert_sorted_indices = (
-            self.__sort_probabilities_and_indices_by_expert_order(
-                probabilities, indexes
-            )
-        )
-        scattered_probabilities = self.__scatter_probabilities(probabilities, indexes)
-        expert_frequency = self.__compute_expert_frequency(probabilities, indexes)
-        expert_sorted_nonzero_probability_indices = (
-            self.__sort_nonzero_probability_indices_by_expert_order(
-                probabilities, indexes
-            )
-        )
-
-        return (
-            expert_sorted_indices,
-            expert_sroted_probabilities,
-            scattered_probabilities,
-            expert_frequency,
-            expert_sorted_nonzero_probability_indices,
-        )
-
-    def __sort_probabilities_and_indices_by_expert_order(
-        self,
-        probabilities: Tensor,
-        indices: Tensor,
-    ) -> tuple[Tensor, Tensor]:
-        (
-            _,
-            flattened_probabilities,
-            nonzero_probability_indices,
-            _expert_sorted_nonzero_indices,
-        ) = self.__required_inputs(probabilities, indices)
-        expert_sorted_nonzero_probability_indices = nonzero_probability_indices[
-            _expert_sorted_nonzero_indices
-        ]
-        expert_sorted_nonzero_indices = expert_sorted_nonzero_probability_indices.div(
-            self.top_k, rounding_mode="trunc"
-        )
-        expert_sorted_nonzero_probabilities = flattened_probabilities[
-            expert_sorted_nonzero_probability_indices
-        ]
-        return expert_sorted_nonzero_probabilities, expert_sorted_nonzero_indices
-
-    def __scatter_probabilities(
-        self,
-        probabilities: Tensor,
-        indices: Tensor,
-    ) -> Tensor:
-        batch_size = probabilities.shape[0]
-        placeholder = torch.zeros([batch_size, self.num_experts], device=device)
-        return placeholder.scatter(1, indices, probabilities)
-
-    def __compute_expert_frequency(self, probabilities, indices):
-        scattered_probabilities = self.__scatter_probabilities(probabilities, indices)
-        expert_frequency = scattered_probabilities > 0
-        expert_frequency = expert_frequency.long()
-        return expert_frequency.sum(dim=0)
-
-    def __sort_nonzero_probability_indices_by_expert_order(
-        self,
-        probabilities: Tensor,
-        indices: Tensor,
-    ) -> Tensor:
-        _, _, nonzero_probability_indices, _expert_sorted_nonzero_indices = (
-            self.__required_inputs(probabilities, indices)
-        )
-
-        return nonzero_probability_indices[_expert_sorted_nonzero_indices]
-
-    def __required_inputs(
-        self,
-        probabilities: Tensor,
-        indices: Tensor,
-    ) -> tuple[Tensor, Tensor, Tensor, Tensor]:
-        flattened_indices = indices.flatten()
-        flattened_probabilities = probabilities.flatten()
-
-        nonzero_probability_indices = flattened_probabilities.nonzero()
-        nonzero_probability_indices = nonzero_probability_indices.squeeze(-1)
-
-        nonzero_indices = flattened_indices[nonzero_probability_indices]
-        _, _expert_sorted_nonzero_indices = nonzero_indices.sort(0)
-
-        return (
-            flattened_indices,
-            flattened_probabilities,
-            nonzero_probability_indices,
-            _expert_sorted_nonzero_indices,
-        )
-
-
 @dataclass
 class ExpertsConfig(DataClassBase):
     input_dim: int | None = field(
@@ -315,12 +181,11 @@ class ExpertsConfig(DataClassBase):
     )
 
 
-class ParallelExperts(Module):
+class ExpertsLayer(Module):
     def __init__(
         self,
         cfg: "ExpertsConfig | ModelConfig",
         overrides: "ExpertsConfig | None" = None,
-        is_output_expert: bool = False,
     ):
         super().__init__()
         config = getattr(cfg, "expert_model_config", cfg)
@@ -329,38 +194,43 @@ class ParallelExperts(Module):
         self.input_dim = self.cfg.input_dim
         self.output_dim = self.cfg.output_dim
         self.num_experts = self.cfg.num_experts
-        self.activation: ActivationFunctionOptions = self.cfg.activation.value
-        self.model_type: LayerTypes = self.cfg.model_type.value
+        self.activation = self.cfg.activation
+        self.model_type = self.cfg.model_type
         self._valudate_fields(self.cfg, ExpertsConfig)
+
         self.experts_module = self.__create_experts()
 
     def __create_experts(self) -> nn.ModuleList:
         expert_list = []
         for _ in range(self.num_experts):
-            model = self.model_type(self.cfg)
+            model = self.model_type.value(self.cfg)
             layer_block = LayerBlock(
                 model=model,
-                activation_function=self.activation,
+                activation_function=self.activation.value,
                 dropout_probability=0.0,
                 layer_norm_flag=True,
             )
             expert_list.append(layer_block)
         return nn.ModuleList(expert_list)
 
-    def forward(self, expert_ordered_input, expert_frequency):
-        expert_inputs = self.__split_input_experts(
-            expert_ordered_input, expert_frequency
-        )
+    def forward(self, input_batch: Tensor, indices: Tensor) -> Tensor:
         expert_outputs = []
         for expert_index in range(self.num_experts):
-            input_tensor = expert_inputs[expert_index]
-            output = self.experts[expert_index](input_tensor)
+            indices_for_expert = self.__get_expert_indices(indices, expert_index)
+            expert_assigned_samples = input_batch[indices_for_expert]
+            output = self.experts[expert_index](expert_assigned_samples)
             expert_outputs.append(output)
         return torch.cat(expert_outputs, dim=0)
 
-    def __split_input_experts(self, expert_ordered_input, expert_frequency):
-        expert_frequency_list = expert_frequency.tolist()
-        return expert_ordered_input.split(expert_frequency_list)
+    def __get_expert_indices(
+        self,
+        indices: Tensor,
+        expert_index: int,
+    ) -> Tensor:
+        boolean_tensor = indices == expert_index
+        flattened_tensor = boolean_tensor.sum(dim=-1)
+        indices_for_expert = flattened_tensor.nonzero()
+        return indices_for_expert
 
 
 class MixtureOfAttentionHeads(MixtureOfExperts):
