a791c99d90ede01c308bd4d0413a2142a6ff1da2
AlphaEmperorOmega <alpha.emperor.omega@gmail.com>
2025-07-20 22:52:46 +0200

    [UPD]: Added unit test experts to unit tests

--- tests.py
+++ tests.py
@@ -1,18 +1,13 @@
 import unittest
 
-# from tests.parameter_generators.generator_choice import *
-# from mperor.tests.library.torch import *
-# from tests.attention.attention import *
-
 from tests.test_routers import *
-
 from tests.test_samplers import *
 from tests.test_mixtures import *
 from tests.test_layers import *
 from tests.test_behaviours import *
 from tests.test_linears import *
 from tests.test_losses import *
-
+from tests.test_experts import *
 
 if __name__ == "__main__":
     unittest.main()
