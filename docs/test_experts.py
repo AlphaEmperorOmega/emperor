cacc879b0e966806ea906b4fdb36d179ed45c1cc
AlphaEmperorOmega <alpha.emperor.omega@gmail.com>
2025-07-21 21:55:49 +0200

    [UPD]: MixtureOfExperts module unit test progress #76

--- docs/test_experts.py
+++ docs/test_experts.py
@@ -7,6 +7,7 @@ from Emperor.experts.experts import (
     ExpertsModule,
     ExpertsModuleConfig,
     MixtureOfExpertsConfig,
+    MixtureOfExperts,
 )
 from Emperor.layers.layers import ParameterLayerConfig
 from Emperor.layers.utils.base import LayerBlock
@@ -105,7 +106,7 @@ class TestExpertsModule(unittest.TestCase):
                 time_tracker_flag=False,
                 dynamic_diagonal_params_flag=False,
             ),
-            moe_config=MixtureOfExpertsConfig(
+            mixture_of_experts_config=MixtureOfExpertsConfig(
                 weighted_parameters_flag=True,
             ),
             input_moe_layer_config=ExpertsModuleConfig(
@@ -415,3 +416,201 @@ class TestExpertsModule(unittest.TestCase):
             self.assertEqual(
                 list(expert_outputs.shape), [batch_size * top_k, config.output_dim]
             )
+
+
+class TestMixtureOfExperts(unittest.TestCase):
+    def setUp(self):
+        # MODEL WISE CONFI
+        BATCH_SIZE = 2
+        INPUT_DIM = 4
+        HIDDEN_DIM = 5
+        OUTPUT_DIM = 6
+        GATHER_FREQUENCY_FLAG = False
+
+        # PARAMETER GENRETOR ROUTER OPITONS
+        ROUTER_INPUT_DIM = HIDDEN_DIM
+        ROUTER_HIDDEN_DIM = 8
+        ROUTER_OUTPUT_DIM = 9
+        ROUTER_NOISY_TOPK_FLAG = False
+        ROUTER_ACTIVATION_FUNCTION = nn.ReLU()
+        ROUTER_NUM_LAYERNUM_LAYERSS = 5
+        ROUTER_DYNAMIC_LINEAR_MODEL_FLAG = False
+
+        # PARAMETER GENRETOR SAMPLER OPITONS
+        SAMPLER_TOP_K = 3
+        SAMPLER_THRESHOLD = 0.0
+        SAMPLER_FILTER_THRESHOLD = False
+        SAMPLER_DYNAMIC_TOPK_THRESHOLD = (1 / ROUTER_OUTPUT_DIM) * 1e-6
+        SAMPLER_NUM_TOPK_SAMPLES = 0
+        SAMPLER_NORMALIZE_PROBABILITIES_FLAG = False
+        SAMPLER_NOISY_TOPK_FLAG = ROUTER_NOISY_TOPK_FLAG
+        SAMPLER_ROUTER_OUTPUT_DIM = ROUTER_OUTPUT_DIM
+        SAMPLER_COEFFICIENT_OF_VARIATION_WEIGHT = 0.1
+        SAMPLER_SWITCH_WEIGHT = 0.1
+        SAMPLER_ZERO_CENTRED_WEIGHT = 0.1
+        SAMPLER_MUTUAL_INFORMATION_WEIGHT = 0.0
+
+        # PARAMETER GENRETOR MIXTURE OPITONS
+        MIXTURE_INPUT_DIM = ROUTER_INPUT_DIM
+        MIXTURE_OUTPUT_DIM = ROUTER_OUTPUT_DIM
+        MIXTURE_DEPTH_DIM = ROUTER_OUTPUT_DIM
+        MIXTURE_TOP_K = SAMPLER_TOP_K
+        MIXTURE_WEIGHTED_PARAMETERS_FLAG = False
+        MIXTURE_BIAS_PARAMETERS_FLAG = False
+        MIXTURE_ROUTER_OUTPUT_DIM = ROUTER_OUTPUT_DIM
+        MIXTURE_CROSS_DIAGONAL_FLAG = False
+
+        # PARAMETER GENERATOR OPTIONS
+        PARAMETER_GENERATOR_BIAS_PARAMETER_FLAG = MIXTURE_WEIGHTED_PARAMETERS_FLAG
+
+        self.cfg = ModelConfig(
+            batch_size=BATCH_SIZE,
+            input_dim=INPUT_DIM,
+            hidden_dim=HIDDEN_DIM,
+            output_dim=OUTPUT_DIM,
+            gather_frequency_flag=GATHER_FREQUENCY_FLAG,
+            router_model_config=RouterConfig(
+                input_dim=ROUTER_INPUT_DIM,
+                hidden_dim=ROUTER_HIDDEN_DIM,
+                output_dim=ROUTER_OUTPUT_DIM,
+                noisy_topk_flag=ROUTER_NOISY_TOPK_FLAG,
+                activation=ROUTER_ACTIVATION_FUNCTION,
+                num_layers=ROUTER_NUM_LAYERNUM_LAYERSS,
+                diagonal_linear_model_flag=ROUTER_DYNAMIC_LINEAR_MODEL_FLAG,
+                residual_flag=False,
+            ),
+            sampler_model_config=SamplerConfig(
+                top_k=SAMPLER_TOP_K,
+                threshold=SAMPLER_THRESHOLD,
+                filter_above_threshold=SAMPLER_FILTER_THRESHOLD,
+                num_topk_samples=SAMPLER_NUM_TOPK_SAMPLES,
+                normalize_probabilities_flag=SAMPLER_NORMALIZE_PROBABILITIES_FLAG,
+                noisy_topk_flag=SAMPLER_NOISY_TOPK_FLAG,
+                num_experts=SAMPLER_ROUTER_OUTPUT_DIM,
+                coefficient_of_variation_loss_weight=SAMPLER_COEFFICIENT_OF_VARIATION_WEIGHT,
+                switch_loss_weight=SAMPLER_SWITCH_WEIGHT,
+                zero_centred_loss_weight=SAMPLER_ZERO_CENTRED_WEIGHT,
+                mutual_information_loss_weight=SAMPLER_MUTUAL_INFORMATION_WEIGHT,
+            ),
+            mixture_model_config=MixtureConfig(
+                input_dim=MIXTURE_INPUT_DIM,
+                output_dim=MIXTURE_OUTPUT_DIM,
+                depth_dim=MIXTURE_DEPTH_DIM,
+                top_k=MIXTURE_TOP_K,
+                bias_parameters_flag=MIXTURE_BIAS_PARAMETERS_FLAG,
+                weighted_parameters_flag=MIXTURE_WEIGHTED_PARAMETERS_FLAG,
+                num_experts=MIXTURE_ROUTER_OUTPUT_DIM,
+                dynamic_diagonal_params_flag=MIXTURE_CROSS_DIAGONAL_FLAG,
+            ),
+            parameter_generator_model_config=ParameterLayerConfig(
+                bias_parameters_flag=PARAMETER_GENERATOR_BIAS_PARAMETER_FLAG,
+                time_tracker_flag=False,
+                dynamic_diagonal_params_flag=False,
+            ),
+            mixture_of_experts_config=MixtureOfExpertsConfig(
+                weighted_parameters_flag=True,
+            ),
+            input_moe_layer_config=ExpertsModuleConfig(
+                input_dim=ROUTER_INPUT_DIM,
+                output_dim=64,
+                dropout_probability=0.1,
+                layer_norm_flag=True,
+                activation=ActivationFunctionOptions.GELU,
+                model_type=LayerTypes.DYNAMIC_BASE,
+                num_experts=SAMPLER_ROUTER_OUTPUT_DIM,
+            ),
+            output_moe_layer_config=ExpertsModuleConfig(
+                input_dim=64,
+                output_dim=ROUTER_INPUT_DIM,
+                dropout_probability=0.1,
+                layer_norm_flag=True,
+                activation=ActivationFunctionOptions.GELU,
+                model_type=LayerTypes.DYNAMIC_BASE,
+                num_experts=SAMPLER_ROUTER_OUTPUT_DIM,
+            ),
+        )
+
+    def test__init_input_layer_with_default_config(self):
+        c = copy.deepcopy(self.cfg)
+        config = c.mixture_of_experts_config
+        m = MixtureOfExperts(c)
+
+        self.assertIsInstance(m, MixtureOfExperts)
+        self.assertEqual(m.weighted_parameters_flag, config.weighted_parameters_flag)
+
+    def test__prepare_inputs(self):
+        c = copy.deepcopy(self.cfg)
+        m = MixtureOfExperts(c)
+
+        batch_size = 5
+        sequence_length = 6
+        embedding_dim = 7
+        input_batch = torch.randn(batch_size, sequence_length, embedding_dim)
+        output, skip_mask = m._MixtureOfExperts__prepare_inputs(input_batch)
+        output_combined_dims, output_embedding_dim = output.shape
+
+        self.assertEqual(output_combined_dims, batch_size * sequence_length)
+        self.assertEqual(output_embedding_dim, embedding_dim)
+        self.assertIsNone(skip_mask)
+
+    def test__prepare_inputs__with__skip_mask(self):
+        c = copy.deepcopy(self.cfg)
+        m = MixtureOfExperts(c)
+
+        batch_size = 5
+        sequence_length = 6
+        embedding_dim = 7
+        input_batch = torch.randn(batch_size, sequence_length, embedding_dim)
+        skip_mask = torch.randint(0, 2, (batch_size, sequence_length))
+        output, reshaped_skip_ask = m._MixtureOfExperts__prepare_inputs(
+            input_batch, skip_mask
+        )
+        output_combined_dims, output_embedding_dim = output.shape
+
+        self.assertEqual(output_combined_dims, batch_size * sequence_length)
+        self.assertEqual(output_embedding_dim, embedding_dim)
+        self.assertTrue(torch.allclose(reshaped_skip_ask, skip_mask.view(-1, 1)))
+
+    def test__resolve_output_shape__2d_input_tensor(self):
+        c = copy.deepcopy(self.cfg)
+        m = MixtureOfExperts(c)
+
+        batch_size = 5
+        embedding_dim = 7
+        input_batch = torch.randn(batch_size, embedding_dim)
+        m._MixtureOfExperts__resolve_output_shape(input_batch)
+
+        self.assertEqual(m.batch_size, batch_size)
+        self.assertEqual(m.sequence_length, 1)
+        self.assertEqual(m.output_shape, [batch_size, -1])
+
+    def test__resolve_output_shape__3d_input_tensor(self):
+        c = copy.deepcopy(self.cfg)
+        m = MixtureOfExperts(c)
+
+        batch_size = 5
+        sequence_length = 6
+        embedding_dim = 7
+        input_batch = torch.randn(batch_size, sequence_length, embedding_dim)
+        m._MixtureOfExperts__resolve_output_shape(input_batch)
+
+        self.assertEqual(batch_size, m.batch_size)
+        self.assertEqual(sequence_length, m.sequence_length)
+        self.assertEqual(m.output_shape, [batch_size, sequence_length, -1])
+
+    def test__compute_expert_mixture__weighted_parameters_flag__False(self):
+        c = copy.deepcopy(self.cfg)
+        overrides = MixtureOfExpertsConfig(
+            weighted_parameters_flag=False,
+        )
+        m = MixtureOfExperts(c, overrides)
+
+        top_k = 3
+        batch_size = 5
+        sequence_length = 6
+        embedding_dim = 7
+        input_batch = torch.randn(top_k * batch_size * sequence_length, embedding_dim)
+
+        output = m._MixtureOfExperts__compute_expert_mixture(input_batch)
+
+        self.assertEqual(batch_size, m.batch_size)
