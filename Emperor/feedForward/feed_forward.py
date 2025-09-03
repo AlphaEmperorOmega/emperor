6165ae0393a2a800e2e5d858dd182d62f0bded1e
AlphaEmperorOmega <alpha.emperor.omega@gmail.com>
2025-09-03 22:56:29 +0200

    [UPD]: Added FeedForward base model for transformer model #81

--- Emperor/feedForward/feed_forward.py
+++ Emperor/feedForward/feed_forward.py
@@ -3,6 +3,7 @@ from dataclasses import dataclass, field
 
 from Emperor.base.utils import DataClassBase, Module
 from Emperor.experts.experts import MixtureOfExperts
+from Emperor.layers.utils.base import LayerBlock
 from Emperor.layers.utils.routers import RouterModel
 from Emperor.layers.utils.samplers import SamplerModel
 
@@ -10,6 +11,103 @@ from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from Emperor.config import ModelConfig
+
+
+@dataclass
+class FeedForwardConfig(DataClassBase):
+    weighted_parameters_flag: bool | None = field(
+        default=None,
+        metadata={
+            "help": "When `True` the sepected parameters will be multiplied by their probs."
+        },
+    )
+
+
+class FeedForward(Module):
+    def __init__(
+        self,
+        cfg: "FeedForwardConfig | ModelConfig",
+        overrides: "FeedForwardConfig | None" = None,
+    ) -> None:
+        super().__init__()
+        config = getattr(cfg, "mixture_of_experts_config", cfg)
+        self.cfg: FeedForwardConfig = self._overwrite_config(config, overrides)
+        self.weighted_parameters_flag = self.cfg.weighted_parameters_flag
+
+        self.input_module = self._create_model(cfg)
+        self.output_module = self._create_model(cfg, is_output_layer_flag=True)
+
+        self.batch_size = None
+        self.sequence_length = None
+        self.output_shape = None
+
+    def _create_model(
+        self,
+        is_output_layer_flag: bool = False,
+    ) -> LayerBlock:
+        return LayerBlock(
+            self.model_type.value(self.cfg),
+            residual_connection_flag=True,
+            dropout_probability=self.dropout_probability,
+            layer_form_first_flag=self.layer_norm_first_flag,
+        )
+
+    def _create_multi_layer(self, cfg: "ModelConfig") -> nn.ModuleList:
+        expert_list = []
+        for _ in range(self.num_experts):
+            model = self.model_type.value(self.__resolve_model_type_overrides(cfg))
+            layer_norm_output_dim = self.output_dim if self.layer_norm_flag else None
+            layer_block = LayerBlock(
+                model=model,
+                activation_function=self.activation.value,
+                layer_norm_output_dim=layer_norm_output_dim,
+                dropout_probability=self.dropout_probability,
+            )
+            expert_list.append(layer_block)
+        return nn.ModuleList(expert_list)
+
+    def forward(
+        self,
+        input_batch: Tensor,
+        skip_mask: Tensor | None = None,
+    ) -> tuple[Tensor, Tensor | None, Tensor]:
+        input_batch_matrix, skip_mask = self._prepare_inputs(input_batch, skip_mask)
+        logits = self.router.compute_logit_scores(input_batch_matrix)
+        probabilities, indices, skip_mask, sampler_loss = (
+            self.sampler.sample_probabilities_and_indices(logits, skip_mask)
+        )
+        input_projection, input_loss = self.input_module.compute_expert_outputs(
+            input_batch_matrix, indices
+        )
+        output_projection, output_loss = self.output_module.compute_expert_outputs(
+            input_projection, indices, probabilities
+        )
+        expert_mixture_output = output_projection.view(self.output_shape)
+        loss = sampler_loss + input_loss + output_loss
+        return expert_mixture_output, skip_mask, loss
+
+    def _prepare_inputs(
+        self,
+        input_batch: Tensor,
+        skip_mask: Tensor | None = None,
+    ) -> tuple[Tensor, Tensor | None]:
+        self.__resolve_output_shape(input_batch)
+        input_batch = input_batch.reshape(self.batch_size * self.sequence_length, -1)
+        if skip_mask is not None:
+            skip_mask = skip_mask.view(-1, 1)
+        return input_batch, skip_mask
+
+    def __resolve_output_shape(self, input_batch: Tensor) -> None:
+        input_shape = input_batch.shape
+        if self.batch_size is not None:
+            return
+        if len(input_shape) > 2:
+            self.batch_size, self.sequence_length, _ = input_shape
+            self.output_shape = [self.batch_size, self.sequence_length, -1]
+            return
+        self.sequence_length = 1
+        self.batch_size, _ = input_shape
+        self.output_shape = [self.batch_size, -1]
 
 
 @dataclass
