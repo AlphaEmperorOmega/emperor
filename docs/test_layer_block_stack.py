fb1b65291817a4b4f837d8e70aa8ef73920cb071
AlphaEmperorOmega <alpha.emperor.omega@gmail.com>
2025-09-07 22:40:19 +0200

    [ADD]: Added LayerBlockStact unit tests #86

--- docs/test_layer_block_stack.py
+++ docs/test_layer_block_stack.py
@@ -1,7 +1,15 @@
+import torch
 import unittest
 
 from dataclasses import asdict
-from Emperor.layers.utils.base import LayerBlockStack, LayerBlockStackConfig
+from Emperor.base.enums import ActivationOptions
+from Emperor.layers.utils.base import (
+    LayerBlock,
+    LayerBlockStack,
+    LayerBlockStackConfig,
+)
+
+from Emperor.layers.utils.enums import LayerTypes
 from docs.utils import default_unittest_config
 
 
@@ -25,7 +33,7 @@ class TestLayerBlockStack(unittest.TestCase):
 
     def rebuild_presets(self, config: LayerBlockStackConfig | None = None):
         self.cfg = default_unittest_config()
-        self.config = self.cfg.linear_block_stack_config
+        self.config = self.cfg.layer_block_stack_config
         if config is not None:
             for k in asdict(config):
                 if hasattr(self.config, k) and getattr(config, k) is not None:
@@ -33,29 +41,136 @@ class TestLayerBlockStack(unittest.TestCase):
 
         self.model = LayerBlockStack(self.cfg)
 
-        # self.batch_size = self.cfg.batch_size
-        # self.embedding_dim = self.cfg.embedding_dim
-        # self.target_sequence_length = self.cfg.target_sequence_length
-        # self.source_sequence_length = self.cfg.source_sequence_length
-        # self.num_heads = self.cfg.num_heads
-        # self.head_dim = self.embedding_dim // self.num_heads
+        self.batch_size = self.cfg.batch_size
+
+        self.input_dim = self.config.input_dim
+        self.hidden_dim = self.config.hidden_dim
+        self.output_dim = self.config.input_dim
+        self.num_layers = self.config.num_layers
+
+        self.activation = self.config.activation
+        self.layer_norm_dim = self.config.layer_norm_dim
+        self.residual_flag = self.config.residual_flag
+        self.adaptive_computation_flag = self.config.adaptive_computation_flag
+        self.dropout_probability = self.config.dropout_probability
+        self.layer_norm_position = self.config.layer_norm_position
 
 
-class TestFeedForward__init(TestLayerBlockStack):
-    def test__init_input_layer_with_default_config(self):
+class Test__init(TestLayerBlockStack):
+    def test_init_input_layer_with_default_config(self):
         self.assertIsInstance(self.model, LayerBlockStack)
+
+        self.assertEqual(self.model.model_type, self.config.model_type)
+        self.assertEqual(self.model.activation, self.config.activation)
+        self.assertEqual(self.model.residual_flag, self.config.residual_flag)
+        self.assertEqual(
+            self.model.adaptive_computation_flag, self.config.adaptive_computation_flag
+        )
+        self.assertEqual(
+            self.model.dropout_probability, self.config.dropout_probability
+        )
+        self.assertEqual(
+            self.model.layer_norm_position, self.config.layer_norm_position
+        )
 
         self.assertEqual(self.model.input_dim, self.config.input_dim)
         self.assertEqual(self.model.hidden_dim, self.config.hidden_dim)
         self.assertEqual(self.model.output_dim, self.config.output_dim)
         self.assertEqual(self.model.num_layers, self.config.num_layers)
-        self.assertEqual(self.model.activation, self.config.activation)
-        self.assertEqual(self.model.model_type, self.config.model_type.value)
-        self.assertEqual(self.model.layer_norm_flag, self.config.layer_norm_flag)
-        self.assertEqual(
-            self.model.layer_form_first_flag, self.config.layer_form_first_flag
+
+
+class Test___resolve_model_type_overrides(TestLayerBlockStack):
+    def test_if_config_is_updated_for_linear_model(self):
+        config = LayerBlockStackConfig(
+            model_type=LayerTypes.DYNAMIC_BASE,
         )
-        self.assertEqual(self.model.residual_flag, self.config.residual_flag)
-        self.assertEqual(
-            self.model.adaptive_computation_flag, self.config.adaptive_computation_flag
+        self.rebuild_presets(config)
+
+        input_dim = 8
+        output_dim = 16
+
+        updated_config = self.model._LayerBlockStack__resolve_model_type_overrides(
+            input_dim, output_dim
         )
+        self.assertEqual(updated_config.linear_layer_model_config.input_dim, input_dim)
+        self.assertEqual(
+            updated_config.linear_layer_model_config.output_dim, output_dim
+        )
+
+    def test_if_config_is_updated_for_parameter_generator_model(self):
+        config = LayerBlockStackConfig(
+            model_type=LayerTypes.VECTOR,
+        )
+        self.rebuild_presets(config)
+
+        input_dim = 8
+        output_dim = 16
+
+        updated_config = self.model._LayerBlockStack__resolve_model_type_overrides(
+            input_dim, output_dim
+        )
+
+        self.assertEqual(updated_config.router_model_config.input_dim, input_dim)
+        self.assertEqual(updated_config.mixture_model_config.input_dim, input_dim)
+        self.assertEqual(updated_config.mixture_model_config.output_dim, output_dim)
+
+
+class Test___create_layer(TestLayerBlockStack):
+    def test_if_all_model_types_are_computed(self):
+        for layer_type in LayerTypes:
+            config = LayerBlockStackConfig(
+                model_type=layer_type,
+            )
+            self.rebuild_presets(config)
+
+            model = self.model._LayerBlockStack__create_layer(
+                self.input_dim, self.output_dim, self.residual_flag
+            )
+
+            input = torch.randn(self.batch_size, self.input_dim)
+            output = model(input)
+            if isinstance(output, tuple):
+                output, _ = model(input)
+
+            expected_output = (self.batch_size, self.output_dim)
+
+            self.assertIsInstance(model, LayerBlock)
+            self.assertIsInstance(model.model, layer_type.value)
+            self.assertEqual(model.activation_function, ActivationOptions.GELU.value)
+            self.assertEqual(model.layer_norm_output_dim, None)
+            self.assertEqual(model.residual_connection_flag, self.residual_flag)
+            self.assertEqual(
+                model.is_adaptive_computation, self.adaptive_computation_flag
+            )
+            self.assertEqual(model.dropout_probability, self.dropout_probability)
+            self.assertEqual(output.shape, expected_output)
+
+    def test_if_all_model_types_are_computed_with_true_residual_flag(self):
+        for layer_type in LayerTypes:
+            config = LayerBlockStackConfig(
+                residual_flag=True,
+                model_type=layer_type,
+            )
+            self.rebuild_presets(config)
+
+            model = self.model._LayerBlockStack__create_layer(
+                self.input_dim, self.output_dim, residual_flag=True
+            )
+
+            input = torch.randn(self.batch_size, self.input_dim)
+            output = model(input)
+            if isinstance(output, tuple):
+                output, _ = model(input)
+
+            expected_output = (self.batch_size, self.output_dim)
+
+            self.assertIsInstance(model, LayerBlock)
+            self.assertIsInstance(model.model, layer_type.value)
+            self.assertEqual(model.activation_function, ActivationOptions.GELU.value)
+            self.assertEqual(model.layer_norm_output_dim, None)
+            self.assertEqual(model.residual_connection_flag, self.residual_flag)
+            self.assertEqual(
+                model.is_adaptive_computation, self.adaptive_computation_flag
+            )
+            self.assertEqual(model.dropout_probability, self.dropout_probability)
+            self.assertEqual(output.shape, expected_output)
