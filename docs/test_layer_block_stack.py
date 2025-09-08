c7527d65afd9b49e414d3604b13cc7fc12a98eab
AlphaEmperorOmega <alpha.emperor.omega@gmail.com>
2025-09-08 20:01:28 +0200

    [UPD]: Finished unit tests for LayerBlock #86

--- docs/test_layer_block_stack.py
+++ docs/test_layer_block_stack.py
@@ -1,5 +1,6 @@
 import torch
 import unittest
+import torch.nn as nn
 
 from dataclasses import asdict
 from Emperor.base.enums import ActivationOptions
@@ -7,6 +8,7 @@ from Emperor.layers.utils.base import (
     LayerBlock,
     LayerBlockStack,
     LayerBlockStackConfig,
+    LayerStackAdjustments,
 )
 
 from Emperor.layers.utils.enums import LayerTypes
@@ -45,7 +47,7 @@ class TestLayerBlockStack(unittest.TestCase):
 
         self.input_dim = self.config.input_dim
         self.hidden_dim = self.config.hidden_dim
-        self.output_dim = self.config.input_dim
+        self.output_dim = self.config.output_dim
         self.num_layers = self.config.num_layers
 
         self.activation = self.config.activation
@@ -148,6 +150,8 @@ class Test___create_layer(TestLayerBlockStack):
     def test_if_all_model_types_are_computed_with_true_residual_flag(self):
         for layer_type in LayerTypes:
             config = LayerBlockStackConfig(
+                input_dim=8,
+                output_dim=8,
                 residual_flag=True,
                 model_type=layer_type,
             )
@@ -174,3 +178,230 @@ class Test___create_layer(TestLayerBlockStack):
             )
             self.assertEqual(model.dropout_probability, self.dropout_probability)
             self.assertEqual(output.shape, expected_output)
+
+
+class Test___add_initial_layer(TestLayerBlockStack):
+    def test_no_layer_is_added_when_num_layers_is_one(self):
+        for layer_type in LayerTypes:
+            config = LayerBlockStackConfig(
+                input_dim=8,
+                output_dim=16,
+                num_layers=1,
+                model_type=layer_type,
+            )
+            self.rebuild_presets(config)
+
+            model_list = []
+            adjustment = self.model._LayerBlockStack__add_initial_layer(model_list)
+
+            self.assertEqual(len(model_list), 0)
+            self.assertEqual(adjustment, LayerStackAdjustments.SHARED_INPUT_OUTPUT_DIM)
+
+    def test_no_layer_is_added_when_num_layers_is_three_with_same_input_output_dim(
+        self,
+    ):
+        for layer_type in LayerTypes:
+            config = LayerBlockStackConfig(
+                input_dim=16,
+                output_dim=16,
+                num_layers=1,
+                model_type=layer_type,
+            )
+            self.rebuild_presets(config)
+
+            model_list = []
+            adjustment = self.model._LayerBlockStack__add_initial_layer(model_list)
+
+            self.assertEqual(len(model_list), 0)
+            self.assertEqual(adjustment, LayerStackAdjustments.SHARED_INPUT_OUTPUT_DIM)
+
+    def test_initial_layer_is_added_with_multiple_layers_and_different_input_output_dim(
+        self,
+    ):
+        num_layers_array = [2, 3, 4]
+        for num_layer in num_layers_array:
+            for layer_type in LayerTypes:
+                config = LayerBlockStackConfig(
+                    input_dim=8,
+                    output_dim=16,
+                    num_layers=num_layer,
+                    model_type=layer_type,
+                )
+                self.rebuild_presets(config)
+
+                model_list = []
+                adjustment = self.model._LayerBlockStack__add_initial_layer(model_list)
+                model = nn.Sequential(*model_list)
+
+                input = torch.randn(self.batch_size, self.input_dim)
+                output = model(input)
+                if isinstance(output, tuple):
+                    output, _ = model(input)
+
+                expected_output = (self.batch_size, self.hidden_dim)
+
+                self.assertEqual(
+                    adjustment, LayerStackAdjustments.SEPARATE_INPUT_OUTPUT_DIM
+                )
+                self.assertEqual(len(model_list), 1)
+                self.assertEqual(output.shape, expected_output)
+
+
+class Test___add_hidden_layers(TestLayerBlockStack):
+    def test_no_layer_added_when_num_layers_is_one(self):
+        for layer_type in LayerTypes:
+            config = LayerBlockStackConfig(
+                input_dim=8,
+                output_dim=16,
+                num_layers=1,
+                model_type=layer_type,
+            )
+            self.rebuild_presets(config)
+
+            model_list = []
+            adjustment = LayerStackAdjustments.SHARED_INPUT_OUTPUT_DIM
+            self.model._LayerBlockStack__add_hidden_layers(model_list, adjustment)
+
+            self.assertEqual(len(model_list), 0)
+
+    def test_no_layer_added_when_num_layers_is_two_with_initial_layer_added(self):
+        for layer_type in LayerTypes:
+            config = LayerBlockStackConfig(
+                input_dim=8,
+                output_dim=16,
+                num_layers=2,
+                model_type=layer_type,
+            )
+            self.rebuild_presets(config)
+
+            model_list = []
+            adjustment = LayerStackAdjustments.SEPARATE_INPUT_OUTPUT_DIM
+            self.model._LayerBlockStack__add_hidden_layers(model_list, adjustment)
+
+            self.assertEqual(len(model_list), 0)
+
+    def test_hidden_layers_are_added_multiple_hidden_layers(self):
+        num_layers_array = [2, 3, 4]
+        for num_layer in num_layers_array:
+            for layer_type in LayerTypes:
+                config = LayerBlockStackConfig(
+                    input_dim=8,
+                    output_dim=16,
+                    num_layers=num_layer,
+                    model_type=layer_type,
+                )
+                self.rebuild_presets(config)
+
+                model_list = []
+
+                adjustment = LayerStackAdjustments.SHARED_INPUT_OUTPUT_DIM
+                self.model._LayerBlockStack__add_hidden_layers(model_list, adjustment)
+                model = nn.Sequential(*model_list)
+
+                input = torch.randn(self.batch_size, self.hidden_dim)
+                output = model(input)
+                if isinstance(output, tuple):
+                    output, _ = model(input)
+
+                expected_output = (self.batch_size, self.hidden_dim)
+
+                self.assertEqual(len(model_list), num_layer - adjustment.value)
+                self.assertEqual(output.shape, expected_output)
+
+
+class Test___add_output_layer(TestLayerBlockStack):
+    def test_ensure_input_layer_is_returned_when_num_layers_is_one(self):
+        for layer_type in LayerTypes:
+            config = LayerBlockStackConfig(
+                input_dim=8,
+                output_dim=16,
+                num_layers=1,
+                model_type=layer_type,
+            )
+            self.rebuild_presets(config)
+
+            model_list = []
+            self.model._LayerBlockStack__add_output_layer(model_list)
+            model = model_list[0]
+
+            input = torch.randn(self.batch_size, self.input_dim)
+            output = model(input)
+            if isinstance(output, tuple):
+                output, _ = model(input)
+
+            expected_output = (self.batch_size, self.output_dim)
+
+            self.assertEqual(len(model_list), 1)
+            self.assertEqual(output.shape, expected_output)
+
+    def test_ensure_output_layer_is_added_when_multiple_layers_are_added(self):
+        for layer_type in LayerTypes:
+            config = LayerBlockStackConfig(
+                input_dim=8,
+                output_dim=16,
+                num_layers=2,
+                model_type=layer_type,
+            )
+            self.rebuild_presets(config)
+
+            model_list = []
+            self.model._LayerBlockStack__add_output_layer(model_list)
+            model = model_list[0]
+
+            input = torch.randn(self.batch_size, self.hidden_dim)
+            output = model(input)
+            if isinstance(output, tuple):
+                output, _ = model(input)
+
+            expected_output = (self.batch_size, self.output_dim)
+
+            self.assertEqual(len(model_list), 1)
+            self.assertEqual(output.shape, expected_output)
+
+
+class Test_build_model(TestLayerBlockStack):
+    def test_model_layer_block_returned_when_num_layers_is_one(self):
+        for layer_type in LayerTypes:
+            config = LayerBlockStackConfig(
+                input_dim=8,
+                output_dim=16,
+                num_layers=1,
+                model_type=layer_type,
+            )
+            self.rebuild_presets(config)
+
+            model = self.model.build_model()
+
+            input = torch.randn(self.batch_size, self.input_dim)
+            output = model(input)
+            if isinstance(output, tuple):
+                output, _ = model(input)
+
+            expected_output = (self.batch_size, self.output_dim)
+
+            self.assertIsInstance(model, LayerBlock)
+            self.assertEqual(output.shape, expected_output)
+
+    def test_sequential_is_returned_when_num_layers_is_greater_than_one(self):
+        num_layers_array = [2, 3, 4]
+        for num_layers in num_layers_array:
+            for layer_type in LayerTypes:
+                config = LayerBlockStackConfig(
+                    input_dim=8,
+                    output_dim=16,
+                    num_layers=num_layers,
+                    model_type=layer_type,
+                )
+                self.rebuild_presets(config)
+
+                model = self.model.build_model()
+
+                input = torch.randn(self.batch_size, self.input_dim)
+                output = model(input)
+                if isinstance(output, tuple):
+                    output, _ = model(input)
+
+                expected_output = (self.batch_size, self.output_dim)
+
+                self.assertIsInstance(model, nn.Sequential)
+                self.assertEqual(output.shape, expected_output)
