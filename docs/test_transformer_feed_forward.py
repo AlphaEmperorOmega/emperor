e7131ea537a5d3ec201db48aa61d110d241743bf
AlphaEmperorOmega <alpha.emperor.omega@gmail.com>
2025-09-10 22:15:34 +0200

    [UPD]: Finished Transformer FeedForward unit tests #84

--- docs/test_transformer_feed_forward.py
+++ docs/test_transformer_feed_forward.py
@@ -1,7 +1,9 @@
+import torch
 import unittest
 
 from dataclasses import asdict
 from Emperor.feedForward.feed_forward import FeedForward, FeedForwardConfig
+from Emperor.layers.utils.enums import LayerTypes
 from docs.utils import default_unittest_config
 
 
@@ -14,14 +16,8 @@ class TestFeedForward(unittest.TestCase):
         self.config = None
         self.model = None
         self.batch_size = None
-        self.embedding_dim = None
-        self.target_sequence_length = None
-        self.num_heads = None
-        self.head_dim = None
-        self.query_model = None
-        self.key_model = None
-        self.value_model = None
-        self.qkv_model = None
+        self.input_dim = None
+        self.output_dim = None
 
     def rebuild_presets(self, config: FeedForwardConfig | None = None):
         self.cfg = default_unittest_config()
@@ -34,15 +30,14 @@ class TestFeedForward(unittest.TestCase):
         self.model = FeedForward(self.cfg)
 
         self.batch_size = self.cfg.batch_size
-        # self.embedding_dim = self.cfg.embedding_dim
-        # self.target_sequence_length = self.cfg.target_sequence_length
-        # self.source_sequence_length = self.cfg.source_sequence_length
-        # self.num_heads = self.cfg.num_heads
-        # self.head_dim = self.embedding_dim // self.num_heads
+        first_layer_of_sequential = self.model.model[0].model
+        self.input_dim = first_layer_of_sequential.input_dim
+        layer_layer_of_sequential = self.model.model[-1].model
+        self.output_dim = layer_layer_of_sequential.output_dim
 
 
 class Test___init(TestFeedForward):
-    def test__init_input_layer_with_default_config(self):
+    def test_init_input_layer_with_default_config(self):
         config = FeedForwardConfig(
             num_layers=2,
         )
@@ -50,3 +45,63 @@ class Test___init(TestFeedForward):
         self.assertIsInstance(self.model, FeedForward)
         self.assertEqual(self.model.num_layers, self.config.num_layers)
         self.assertEqual(len(self.model.model), self.config.num_layers)
+
+    def test_ensure_error_is_triggered_if_num_layers_is_one(self):
+        config = FeedForwardConfig(
+            num_layers=1,
+        )
+        with self.assertRaises(RuntimeError):
+            self.rebuild_presets(config)
+
+    def test_differet_layer_types(self):
+        num_layers_list = [2, 4, 6]
+        for layer_type in LayerTypes:
+            for num_layers in num_layers_list:
+                config = FeedForwardConfig(
+                    model_type=layer_type,
+                    num_layers=num_layers,
+                )
+                self.rebuild_presets(config)
+                self.assertIsInstance(self.model.model[0].model, layer_type.value)
+
+
+class Test_forward(TestFeedForward):
+    def test_ensure_the_feed_forward_model_processes_2D_input_batch(self):
+        num_layers_list = [2, 4, 6]
+        for layer_type in LayerTypes:
+            for num_layers in num_layers_list:
+                config = FeedForwardConfig(
+                    model_type=layer_type,
+                    num_layers=num_layers,
+                )
+                self.rebuild_presets(config)
+                input = torch.randn(self.batch_size, self.input_dim)
+                output = self.model(input)
+
+                expected_output = (self.batch_size, self.output_dim)
+
+                if isinstance(output, tuple):
+                    output, _ = output
+
+                self.assertIsInstance(self.model.model[0].model, layer_type.value)
+                self.assertEqual(output.shape, expected_output)
+
+    def test_ensure_the_feed_forward_model_processes_3D_input_batch(self):
+        num_layers_list = [2, 4, 6]
+        for layer_type in LayerTypes:
+            for num_layers in num_layers_list:
+                config = FeedForwardConfig(
+                    model_type=layer_type,
+                    num_layers=num_layers,
+                )
+                self.rebuild_presets(config)
+                sequence_length = 4
+                input = torch.randn(self.batch_size, sequence_length, self.input_dim)
+                output = self.model(input)
+
+                output, _ = output
+
+                expected_output = (self.batch_size, sequence_length, self.output_dim)
+
+                self.assertIsInstance(self.model.model[0].model, layer_type.value)
+                self.assertEqual(output.shape, expected_output)
