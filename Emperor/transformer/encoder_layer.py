d58913985e57db1e61f06dd6789436c36ce80087
AlphaEmperorOmega <alpha.emperor.omega@gmail.com>
2025-09-11 22:05:59 +0200

    [UPD]: Finished transformer encoder layer before unit tests #81

--- Emperor/transformer/encoder_layer.py
+++ Emperor/transformer/encoder_layer.py
@@ -1,18 +1,18 @@
-# import torch.nn as nn
+import torch.nn as nn
 # nn.TransformerEncoderLayer
 
 from torch import Tensor
 from dataclasses import dataclass, field
-
-
 from Emperor.attention.attention import MultiHeadAttention
+from Emperor.base.enums import LayerNormPositionOptions
 from Emperor.feedForward.feed_forward import FeedForward
+from Emperor.base.utils import DataClassBase, Module
+from Emperor.layers.utils.base import LayerBlock
 from Emperor.layers.utils.enums import (
     AttentionTypes,
     FeedForwardTypes,
     LayerTypes,
 )
-from Emperor.base.utils import DataClassBase, Module
 
 from typing import TYPE_CHECKING
 
@@ -21,7 +21,7 @@ if TYPE_CHECKING:
 
 
 @dataclass
-class TransformerEncoderLayerConfig(DataClassBase):
+class TransformerLayerConfig(DataClassBase):
     model_type: LayerTypes | None = field(
         default=None,
         metadata={"help": ""},
@@ -38,53 +38,50 @@ class TransformerEncoderLayerConfig(DataClassBase):
             "help": "Type of feed forward model used to compute the transformer feed forward output"
         },
     )
-    qkv_projection_dim: int | None = field(
-        default=None,
-        metadata={
-            "help": "Dimension of query, key, value hidden projections of the attention mechanism."
-        },
-    )
-    feed_forward_projection_dim: int | None = field(
-        default=None,
-        metadata={
-            "help": "Dimension applies to the feed forward module input hidden dimension."
-        },
-    )
-    layer_norm_eps: float | None = field(
-        default=None,
-        metadata={"help": ""},
-    )
-    layer_norm_first_flag: bool | None = field(
-        default=None,
-        metadata={"help": ""},
-    )
     dropout_probability: float | None = field(
         default=None,
-        metadata={
-            "help": "Dropout probability applied to attention weights (prevents overfitting)."
-        },
+        metadata={"help": ""},
+    )
+    layer_norm_position: "LayerNormPositionOptions | None" = field(
+        default=None,
+        metadata={"help": ""},
+    )
+    layer_norm_position: "LayerNormPositionOptions | None" = field(
+        default=None,
+        metadata={"help": ""},
     )
 
 
-class TransformerEncoderLayer(Module):
+class TransformerLayerBase(Module):
+    def _create_model(self, model: MultiHeadAttention | FeedForward) -> LayerBlock:
+        return LayerBlock(
+            model=model(cfg),
+            residual_connection_flag=True,
+            layer_norm_dim=self.layer_norm_dim,
+            layer_norm_position=self.layer_norm_position,
+            dropout_probability=self.dropout_probability,
+        )
+
+
+class TransformerEncoderLayer(TransformerLayerBase):
     def __init__(
         self,
-        cfg: "TransformerEncoderLayerConfig | ModelConfig",
-        overrides: "TransformerEncoderLayerConfig | None" = None,
+        cfg: "TransformerLayerConfig | ModelConfig",
+        overrides: "TransformerLayerConfig | None" = None,
     ):
         super().__init__()
+        nn.TransformerDecoderLayer
         config = getattr(cfg, "multi_head_attention_model_config", cfg)
-        self.cfg: "TransformerEncoderLayerConfig" = self._overwrite_config(
-            config, overrides
-        )
+        self.cfg: "TransformerLayerConfig" = self._overwrite_config(config, overrides)
         self.main_cfg = cfg
         self.attention_type = self.cfg.attention_type
         self.feed_forward_type = self.cfg.feed_forward_type
         self.dropout_probability = self.cfg.dropout_probability
-        self.layer_norm_first_flag = self.cfg.layer_norm_first_flag
+        self.layer_norm_dim = self.cfg.layer
+        self.layer_norm_position = self.cfg.layer_norm_position
 
-        self.attention_model = MultiHeadAttention(cfg)
-        self.feed_forward_model = FeedForward(cfg)
+        self.attention_model = self._create_model(MultiHeadAttention(cfg))
+        self.feed_forward_model = self._create_model(FeedForward(cfg))
 
     def forward(
         self,
@@ -94,5 +91,35 @@ class TransformerEncoderLayer(Module):
     ) -> Tensor:
         other_attention_inputs = (key_padding_mask, attention_mask)
         x = self.attention_model(input_tensor, other_attention_inputs)
+        x = self.feed_forward_model(x)
+        return x
+
+
+class TransformerDecoderLayer(TransformerLayerBase):
+    def __init__(
+        self,
+        cfg: "TransformerLayerConfig | ModelConfig",
+        overrides: "TransformerLayerConfig | None" = None,
+    ):
+        super().__init__()
+        config = getattr(cfg, "multi_head_attention_model_config", cfg)
+        self.cfg: "TransformerLayerConfig" = self._overwrite_config(config, overrides)
+        self.main_cfg = cfg
+        self.attention_type = self.cfg.attention_type
+        self.feed_forward_type = self.cfg.feed_forward_type
+
+        self.self_attention_model = self._create_model(MultiHeadAttention(cfg))
+        self.cross_attention_model = self._create_model(MultiHeadAttention(cfg))
+        self.feed_forward_model = self._create_model(FeedForward(cfg))
+
+    def forward(
+        self,
+        input_tensor: Tensor,
+        key_padding_mask: Tensor | None = None,
+        attention_mask: Tensor | None = None,
+    ) -> Tensor:
+        other_attention_inputs = (key_padding_mask, attention_mask)
+        x = self.self_attention_model(input_tensor, other_attention_inputs)
+        x = self.cross_attention_model(x, other_attention_inputs)
         x = self.feed_forward_model(x)
         return x
