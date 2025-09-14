6ced37490d923fb20c0142f2971894cdd33181ab
AlphaEmperorOmega <alpha.emperor.omega@gmail.com>
2025-09-14 22:04:51 +0200

    [UPD]: Added MultiHeadAttentionLayerBlock

--- Emperor/layers/utils/base.py
+++ Emperor/layers/utils/base.py
@@ -106,8 +106,7 @@ class LayerBlock(Module):
         if self.layer_norm_position == LayerNormPositionOptions.BEFORE:
             main_model_input = self.layer_norm_module(main_model_input)
 
-        model_output = self.model(main_model_input, **additional_model_inputs)
-        output = self._handle_model_output(model_output)
+        output = self._handle_model_output(main_model_input, additional_model_inputs)
 
         if self.layer_norm_position == LayerNormPositionOptions.DEFAULT:
             output = self.layer_norm_module(output)
@@ -127,8 +126,12 @@ class LayerBlock(Module):
     def _handle_model_input(self, main_model_input: Tensor):
         return main_model_input
 
-    def _handle_model_output(self, model_output: Tensor) -> Tensor:
-        return model_output
+    def _handle_model_output(
+        self,
+        main_model_input: Tensor,
+        additional_model_inputs: dict,
+    ) -> Tensor:
+        return self.model(main_model_input, **additional_model_inputs)
 
     def _handle_final_output(self, output: Tensor) -> Tensor:
         return output
@@ -174,7 +177,65 @@ class ParameterGeneratorLayerBlock(LayerBlock):
             self.loss = previous_loss
         return main_model_input
 
-    def _handle_model_output(self, model_output: tuple) -> Tensor:
+    def _handle_model_output(
+        self,
+        main_model_input: Tensor,
+        additional_model_inputs: dict,
+    ) -> Tensor:
+        model_output = self.model(
+            main_model_input,
+            **additional_model_inputs,
+        )
+        output, skip_mask, loss = model_output
+        self.loss = self.loss + loss
+        return output
+
+    def _handle_final_output(self, output: Tensor) -> tuple[Tensor, Tensor]:
+        return output, self.loss
+
+
+class MultiHeadAttentionLayerBlock(LayerBlock):
+    def __init__(
+        self,
+        model: "Module",
+        activation_function: "ActivationOptions | None" = None,
+        layer_norm_dim: int | None = None,
+        residual_connection_flag: bool = False,
+        is_adaptive_computation: bool = False,
+        dropout_probability: float = 0.0,
+        layer_norm_position: "LayerNormPositionOptions | None" = None,
+    ):
+        super().__init__(
+            model,
+            activation_function,
+            layer_norm_dim,
+            residual_connection_flag,
+            is_adaptive_computation,
+            dropout_probability,
+            layer_norm_position,
+        )
+
+        self.loss = torch.tensor(0.0)
+
+    def _handle_model_input(
+        self, main_model_input: Tensor | tuple[Tensor, Tensor]
+    ) -> Tensor:
+        if isinstance(main_model_input, tuple):
+            main_model_input, previous_loss = main_model_input
+            self.loss = previous_loss
+        return main_model_input
+
+    def _handle_model_output(
+        self,
+        main_model_input: Tensor,
+        additional_model_inputs: dict,
+    ) -> Tensor:
+        model_output = self.model(
+            main_model_input,
+            main_model_input,
+            main_model_input,
+            **additional_model_inputs,
+        )
         output, skip_mask, loss = model_output
         self.loss = self.loss + loss
         return output
