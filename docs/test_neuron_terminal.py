0f96d4d803208da961683caf8a882ca6aa3587a6
AlphaEmperorOmega <alpha.emperor.omega@gmail.com>
2025-10-26 22:32:15 +0100

    [UPD]: Updated neuron terminals unit tests

--- docs/test_neuron_terminal.py
+++ docs/test_neuron_terminal.py
@@ -1,8 +1,16 @@
+import random
 import unittest
 
+from torch import Tensor
 from dataclasses import asdict
-
-from Emperor.neuron.neuron import Terminal, TerminalConfig
+from Emperor.layers.utils.routers import RouterModel
+from Emperor.layers.utils.samplers import SamplerModel
+from Emperor.neuron.neuron import (
+    Terminal,
+    TerminalConfig,
+    TerminalRangeOptions,
+    TerminalZAxisOffsetOptions,
+)
 from docs.config import default_unittest_config
 
 
@@ -38,27 +46,179 @@ class Test___init(TestNeuronTerminal):
         self.assertEqual(self.model.x_axis_position, self.config.x_axis_position)
         self.assertEqual(self.model.y_axis_position, self.config.y_axis_position)
         self.assertEqual(self.model.z_axis_position, self.config.z_axis_position)
-        self.assertEqual(self.model.x_axis_range, self.config.x_axis_range)
-        self.assertEqual(self.model.y_axis_range, self.config.y_axis_range)
-        self.assertEqual(self.model.z_axis_range, self.config.z_axis_range)
+        self.assertEqual(self.model.xy_axis_range, self.config.xy_axis_range.value)
+        self.assertEqual(self.model.z_axis_range, self.config.z_axis_range.value)
+        self.assertIsInstance(self.model.router_model, RouterModel)
+        self.assertIsInstance(self.model.sampler_model, SamplerModel)
+        self.assertIsInstance(self.model.neuron_connections, Tensor)
+        self.assertEqual(
+            len(self.model.neuron_connections), self.model.total_neuron_connections
+        )
 
 
 class Test___compute_xy_axis_range(TestNeuronTerminal):
     def test_x_axis_range(self):
-        range_options = [2, 4, 8, 16]
-        position_options = [0, 1, 2, 3]
-        for option in range_options:
-            for position in position_options:
+        num_positions = 3
+        for xyz_range in TerminalRangeOptions:
+            for _ in range(num_positions):
+                x_position = random.randint(0, 10)
+                y_position = random.randint(0, 10)
+                z_position = random.randint(0, 10)
                 config = TerminalConfig(
-                    x_axis_position=position,
-                    y_axis_position=position,
-                    z_axis_position=position,
-                    x_axis_range=option,
-                    y_axis_range=option,
-                    z_axis_range=option,
+                    x_axis_position=x_position,
+                    y_axis_position=y_position,
+                    z_axis_position=z_position,
+                    xy_axis_range=xyz_range,
+                    z_axis_range=xyz_range,
+                    z_axis_offset=TerminalZAxisOffsetOptions.ZERO,
                 )
                 self.rebuild_presets(config=config)
-                message = f"__compute_xy_axis_range at xyz position: ({position}) with xyz range: {option}"
+                message = f"__compute_xy_axis_range at xyz position: ({x_position}, {y_position}, {z_position}) with xyz range: {xyz_range.value}"
                 with self.subTest(msg=message):
-                    x_axis_range_indices = self.model._Terminal__compute_xy_axis_range()
-                    print(x_axis_range_indices)
+                    axis_range_indices = self.model._Terminal__compute_xy_axis_range()
+                    expected_range_length = self.model.xy_axis_range * 2 + 1
+                    expected_range_length = self.model.xy_axis_range * 2 + 1
+                    expected_range_start = x_position - self.model.xy_axis_range
+                    expected_range_end = x_position + self.model.xy_axis_range
+                    self.assertIsInstance(axis_range_indices, Tensor)
+                    self.assertEqual(axis_range_indices.numel(), expected_range_length)
+                    self.assertEqual(axis_range_indices[0].item(), expected_range_start)
+                    self.assertEqual(axis_range_indices[-1].item(), expected_range_end)
+
+    def test_y_axis_range(self):
+        num_positions = 3
+        for xyz_range in TerminalRangeOptions:
+            for _ in range(num_positions):
+                x_position = random.randint(0, 10)
+                y_position = random.randint(0, 10)
+                z_position = random.randint(0, 10)
+                config = TerminalConfig(
+                    x_axis_position=x_position,
+                    y_axis_position=y_position,
+                    z_axis_position=z_position,
+                    xy_axis_range=xyz_range,
+                    z_axis_range=xyz_range,
+                    z_axis_offset=TerminalZAxisOffsetOptions.ZERO,
+                )
+                self.rebuild_presets(config=config)
+                message = f"__compute_xy_axis_range at xyz position: ({x_position}, {y_position}, {z_position}) with xy_axis_range: {xyz_range.value}"
+                with self.subTest(msg=message):
+                    is_y_axis_flag = True
+                    axis_range_indices = self.model._Terminal__compute_xy_axis_range(
+                        is_y_axis_flag
+                    )
+                    expected_range_length = self.model.xy_axis_range * 2 + 1
+                    expected_range_start = y_position - self.model.xy_axis_range
+                    expected_range_end = y_position + self.model.xy_axis_range
+
+                    self.assertIsInstance(axis_range_indices, Tensor)
+                    self.assertEqual(axis_range_indices.numel(), expected_range_length)
+                    self.assertEqual(axis_range_indices[0].item(), expected_range_start)
+                    self.assertEqual(axis_range_indices[-1].item(), expected_range_end)
+
+
+class Test___compute_z_axis_range(TestNeuronTerminal):
+    def test_without_triggering_value_errors(self):
+        num_positions = 3
+        for xyz_range in TerminalRangeOptions:
+            for z_axis_offset in TerminalZAxisOffsetOptions:
+                for _ in range(num_positions):
+                    if xyz_range.value <= 2 and z_axis_offset.value > 0:
+                        continue
+                    if (xyz_range.value - z_axis_offset.value) <= 0:
+                        continue
+                    x_position = random.randint(0, 10)
+                    y_position = random.randint(0, 10)
+                    z_position = random.randint(0, 10)
+                    config = TerminalConfig(
+                        x_axis_position=x_position,
+                        y_axis_position=y_position,
+                        z_axis_position=z_position,
+                        xy_axis_range=xyz_range,
+                        z_axis_range=xyz_range,
+                        z_axis_offset=z_axis_offset,
+                    )
+                    message = f"__compute_z_axis_range at xyz position: ({x_position}, {y_position}, {z_position}) with z range: {xyz_range.value} and z_offset: {z_axis_offset.value}"
+                    self.rebuild_presets(config=config)
+                    with self.subTest(msg=message):
+                        axis_range_indices = (
+                            self.model._Terminal__compute_z_axis_range()
+                        )
+                        z_range = self.model.z_axis_range
+                        z_offset = self.model.z_axis_offset
+                        z_offset = z_offset if z_range > 2 else 0
+                        expected_range_length = z_range + 1
+                        expected_range_start = z_position - z_offset
+                        expected_range_end = z_position + z_range - z_offset
+
+                        self.assertIsInstance(axis_range_indices, Tensor)
+                        self.assertEqual(
+                            axis_range_indices.numel(), expected_range_length
+                        )
+                        self.assertEqual(
+                            axis_range_indices[0].item(), expected_range_start
+                        )
+                        self.assertEqual(
+                            axis_range_indices[-1].item(), expected_range_end
+                        )
+
+    def test_ensure_value_errors_are_triggered(self):
+        num_positions = 3
+        for xyz_range in TerminalRangeOptions:
+            for z_axis_offset in TerminalZAxisOffsetOptions:
+                for _ in range(num_positions):
+                    if not (xyz_range.value <= 2 and z_axis_offset.value > 0):
+                        continue
+                    if not ((xyz_range.value - z_axis_offset.value) <= 0):
+                        continue
+
+                    message = f"__compute_z_axis_range "
+                    with self.subTest(msg=message):
+                        with self.assertRaises(ValueError):
+                            x_position = random.randint(0, 10)
+                            y_position = random.randint(0, 10)
+                            z_position = random.randint(0, 10)
+                            config = TerminalConfig(
+                                x_axis_position=x_position,
+                                y_axis_position=y_position,
+                                z_axis_position=z_position,
+                                xy_axis_range=xyz_range,
+                                z_axis_range=xyz_range,
+                                z_axis_offset=z_axis_offset,
+                            )
+                            self.rebuild_presets(config=config)
+                            self.model._Terminal__compute_z_axis_range()
+
+
+# class Test___compute_z_axis_offset_limit(TestNeuronTerminal):
+#     def test_method(self):
+#         num_positions = 3
+#         for xyz_range in TerminalRangeOptions:
+#             for _ in range(num_positions):
+#                 x_position = random.randint(0, 10)
+#                 y_position = random.randint(0, 10)
+#                 z_position = random.randint(0, 10)
+#                 config = TerminalConfig(
+#                     x_axis_position=x_position,
+#                     y_axis_position=y_position,
+#                     z_axis_position=z_position,
+#                     xy_axis_range=xyz_range,
+#                     z_axis_range=xyz_range,
+#                 )
+#                 message = f"__compute_z_axis_range at xyz position: ({x_position}, {y_position}, {z_position}) with z range: {xyz_range.value} and z_offset: {z_axis_offset.value}"
+#                 self.rebuild_presets(config=config)
+#                 with self.subTest(msg=message):
+#                     axis_range_indices = (
+#                         self.model._Terminal__compute_z_axis_offset_limit()
+#                     )
+#                     z_range = self.model.z_axis_range
+#                     z_offset = self.model.z_axis_offset
+#                     z_offset = z_offset if z_range > 2 else 0
+#                     expected_range_length = z_range + 1
+#                     expected_range_start = z_position - z_offset
+#                     expected_range_end = z_position + z_range - z_offset
+#
+#                     self.assertIsInstance(axis_range_indices, Tensor)
+#                     self.assertEqual(axis_range_indices.numel(), expected_range_length)
+#                     self.assertEqual(axis_range_indices[0].item(), expected_range_start)
+#                     self.assertEqual(axis_range_indices[-1].item(), expected_range_end)
