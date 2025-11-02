b3568247c5ea61727978a1f2f942b5f4929652a4
AlphaEmperorOmega <alpha.emperor.omega@gmail.com>
2025-11-02 22:07:03 +0100

    [UPD]: test.py now uses the new classes

--- test.py
+++ test.py
@@ -1,4 +1,13 @@
-from Emperor.experiments.layers.layers_factories import TrainPresetsWrapper
+# from Emperor.experiments.layers.layers_factories import TrainPresetsWrapper
+#
+# if __name__ == "__main__":
+#     TrainPresetsWrapper().test_all_preset_models(False)
+
+from Emperor.experiments.linears import LinearsExperiments
+
 
 if __name__ == "__main__":
-    TrainPresetsWrapper().test_all_preset_models(False)
+    options = {
+        "mini_datasetset_flag": False,
+    }
+    LinearsExperiments(**options).train_base_model()
