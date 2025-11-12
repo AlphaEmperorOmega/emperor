4f567d7869999ccdec82b2a135f447313c3ee6b0
AlphaEmperorOmega <alpha.emperor.omega@gmail.com>
2025-11-12 09:32:29 +0100

    [UPD]: Generalised the model initialization

--- Emperor/linears/utils/handlers/bias.py
+++ Emperor/linears/utils/handlers/bias.py
@@ -23,6 +23,11 @@ class DefaultBiasHandler(Module):
         self.output_dim = cfg.output_dim
         self.bias_params = bias_params
 
+    def _init_model(
+        self, overrides: LayerStackConfig | None = None
+    ) -> LinearLayerStack:
+        return LinearLayerStack(self.cfg, overrides=overrides)
+
     def forward(self, logits: Tensor) -> Tensor | None:
         return self.bias_params
 
@@ -34,11 +39,8 @@ class AffineBiasTransformHandler(DefaultBiasHandler):
         bias_params: Tensor | None = None,
     ):
         super().__init__(cfg, bias_params)
-        self.scalar_offset_generator = self.__init_scale_and_offset_model()
-
-    def __init_scale_and_offset_model(self) -> LinearLayerStack:
         overrides = LayerStackConfig(output_dim=2)
-        return LinearLayerStack(self.cfg, overrides)
+        self.scalar_offset_generator = self._init_model(overrides)
 
     def forward(self, logits: Tensor) -> Tensor:
         parameters = self.scalar_offset_generator(logits)
@@ -53,10 +55,7 @@ class ElementwiseBiasHandler(DefaultBiasHandler):
         bias_params: Tensor | None = None,
     ):
         super().__init__(cfg, bias_params)
-        self.generator_model = self.__init_scale_and_offset_model()
-
-    def __init_scale_and_offset_model(self) -> LinearLayerStack:
-        return LinearLayerStack(self.cfg)
+        self.generator_model = self._init_model()
 
     def forward(self, logits: Tensor) -> Tensor:
         parameters = self.generator_model(logits)
@@ -71,16 +70,13 @@ class BiasGeneratorHandler(DefaultBiasHandler):
     ):
         super().__init__(cfg, bias_params)
         self.__ensure_no_bias_params_are_given()
-        self.bias_generator = self.__init_bias_generator_model()
+        self.bias_generator = self._init_model()
 
     def __ensure_no_bias_params_are_given(self):
         if self.bias_params is not None:
             raise ValueError(
                 "bias_params must be None for this `BiasGeneratorHandler`."
             )
-
-    def __init_bias_generator_model(self) -> LinearLayerStack:
-        return LinearLayerStack(self.cfg)
 
     def forward(self, logits: Tensor) -> Tensor | None:
         return self.bias_generator(logits)
