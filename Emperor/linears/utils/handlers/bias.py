3b70ef701b2191df474260e14d082d97359f5d9c
AlphaEmperorOmega <alpha.emperor.omega@gmail.com>
2025-11-13 06:22:38 +0100

    [UPD]: Cleaned up linears handlers

--- Emperor/linears/utils/handlers/bias.py
+++ Emperor/linears/utils/handlers/bias.py
@@ -11,72 +11,69 @@ if TYPE_CHECKING:
     from Emperor.config import ModelConfig
 
 
-class DefaultBiasHandler(Module):
+class BiasHandlerAbstract(Module):
     def __init__(
         self,
         cfg: "ModelConfig",
-        bias_params: Tensor | None = None,
     ):
         super().__init__()
-        self.cfg = cfg
-        self.input_dim = cfg.input_dim
-        self.output_dim = cfg.output_dim
-        self.bias_params = bias_params
+        self.cfg = getattr(cfg, "linear_layer_model_config", cfg)
+        self.main_cfg = cfg
+        self.input_dim = self.cfg.input_dim
+        self.output_dim = self.cfg.output_dim
 
     def _init_model(
         self, overrides: LayerStackConfig | None = None
     ) -> LinearLayerStack:
-        return LinearLayerStack(self.cfg, overrides=overrides)
-
-    def forward(self, logits: Tensor) -> Tensor | None:
-        return self.bias_params
+        return LinearLayerStack(self.main_cfg, overrides=overrides)
 
 
-class AffineBiasTransformHandler(DefaultBiasHandler):
+class DefaultBiasHandler(BiasHandlerAbstract):
     def __init__(
         self,
         cfg: "ModelConfig",
-        bias_params: Tensor | None = None,
     ):
-        super().__init__(cfg, bias_params)
+        super().__init__(cfg)
+
+    def forward(self, bias_params: Tensor, logits: None = None) -> Tensor:
+        return bias_params
+
+
+class AffineBiasTransformHandler(BiasHandlerAbstract):
+    def __init__(
+        self,
+        cfg: "ModelConfig",
+    ):
+        super().__init__(cfg)
         overrides = LayerStackConfig(output_dim=2)
         self.scalar_offset_generator = self._init_model(overrides)
 
-    def forward(self, logits: Tensor) -> Tensor:
+    def forward(self, bias_params: Tensor, logits: Tensor) -> Tensor:
         parameters = self.scalar_offset_generator(logits)
         bias_scaling_factor, bias_offset = parameters.chunk(2, dim=-1)
-        return bias_scaling_factor * self.bias_params + bias_offset
+        return bias_scaling_factor * bias_params + bias_offset
 
 
-class ElementwiseBiasHandler(DefaultBiasHandler):
+class ElementwiseBiasHandler(BiasHandlerAbstract):
     def __init__(
         self,
         cfg: "ModelConfig",
-        bias_params: Tensor | None = None,
     ):
-        super().__init__(cfg, bias_params)
+        super().__init__(cfg)
         self.generator_model = self._init_model()
 
-    def forward(self, logits: Tensor) -> Tensor:
+    def forward(self, bias_params: Tensor, logits: Tensor) -> Tensor:
         parameters = self.generator_model(logits)
-        return self.bias_params + parameters
+        return bias_params + parameters
 
 
-class BiasGeneratorHandler(DefaultBiasHandler):
+class BiasGeneratorHandler(BiasHandlerAbstract):
     def __init__(
         self,
         cfg: "ModelConfig",
-        bias_params: None = None,
     ):
-        super().__init__(cfg, bias_params)
-        self.__ensure_no_bias_params_are_given()
+        super().__init__(cfg)
         self.bias_generator = self._init_model()
 
-    def __ensure_no_bias_params_are_given(self):
-        if self.bias_params is not None:
-            raise ValueError(
-                "bias_params must be None for this `BiasGeneratorHandler`."
-            )
-
-    def forward(self, logits: Tensor) -> Tensor | None:
+    def forward(self, bias_params: None, logits: Tensor) -> Tensor | None:
         return self.bias_generator(logits)
--- Emperor/linears/utils/handlers/diagonal.py
+++ Emperor/linears/utils/handlers/diagonal.py
@@ -1,3 +1,6 @@
+import torch
+import torch.nn.functional as F
+
 from torch import Tensor
 from Emperor.base.utils import Module
 from Emperor.base.layer import (
@@ -11,13 +14,14 @@ if TYPE_CHECKING:
     from Emperor.config import ModelConfig
 
 
-class DefaultDiagonalHandler(Module):
+class DiagonalHandlerAbstract(Module):
     def __init__(
         self,
         cfg: "ModelConfig",
     ):
         super().__init__()
-        self.cfg = cfg
+        self.cfg_main = cfg
+        self.cfg = getattr(cfg, "linear_layer_model_config", cfg)
         self.input_dim = cfg.input_dim
         self.output_dim = cfg.output_dim
         self.padding_shape = self.__get_diagonal_padding_shape()
@@ -31,17 +35,17 @@ class DefaultDiagonalHandler(Module):
                 diagonal_padding_shape = (0, 0, 0, padding_size)
         return diagonal_padding_shape
 
-    def _init_diagonal_model(
+    def _init_model(
         self,
     ) -> LinearLayerStack:
         output_dim = min(self.input_dim, self.output_dim)
         overrides = LayerStackConfig(output_dim=output_dim)
-        return LinearLayerStack(self.cfg, overrides)
+        return LinearLayerStack(self.cfg_main, overrides)
 
     def forward(self, weight_params: Tensor) -> Tensor:
         return weight_params
 
-    def _convert_to_diagonal_matrix(
+    def __convert_to_diagonal_matrix(
         self,
         vector_matrix: Tensor,
     ) -> Tensor:
@@ -50,44 +54,50 @@ class DefaultDiagonalHandler(Module):
             diagonal_matrix = F.pad(diagonal_matrix, self.padding_shape)
         return diagonal_matrix
 
-
-class DiagonalHandler(DefaultDiagonalHandler):
-    def __init__(
-        self,
-        cfg: "ModelConfig",
-    ):
-        super().__init__(cfg)
-        self.input_dim = cfg.input_dim
-        self.output_dim = cfg.output_dim
-        self.diagonal_generator = self._init_diagonal_model()
-
-    def forward(self, logits: Tensor, weight_params: Tensor) -> Tensor:
-        diagonal_matrices = self._compute_diagonal_matrices(logits)
-        return weight_params + diagonal_matrices
-
     def _compute_diagonal_matrix(self, logits: Tensor) -> Tensor:
         vectors = self.diagonal_generator(logits)
-        return self._convert_to_diagonal_matrix(vectors)
+        return self.__convert_to_diagonal_matrix(vectors)
 
 
-class AntiDiagonalHandler(DiagonalHandler):
+class DefaultDiagonalHandler(DiagonalHandlerAbstract):
     def __init__(
         self,
         cfg: "ModelConfig",
     ):
         super().__init__(cfg)
-        self.cfg = cfg
-        self.input_dim = cfg.input_dim
-        self.output_dim = cfg.output_dim
-        self.diagonal_generator = self._init_diagonal_model()
 
-    def forward(self, logits: Tensor, weight_params: Tensor) -> Tensor:
-        diagonal_matrices = self._compute_diagonal_matrices(logits)
+    def forward(self, weight_params: Tensor, logits: Tensor) -> Tensor:
+        return weight_params
+
+
+class DiagonalHandler(DiagonalHandlerAbstract):
+    def __init__(
+        self,
+        cfg: "ModelConfig",
+    ):
+        super().__init__(cfg)
+        self.diagonal_generator = self._init_model()
+
+    def forward(self, weight_params: Tensor, logits: Tensor) -> Tensor:
+        diagonal_matrices = self._compute_diagonal_matrix(logits)
+        return weight_params + diagonal_matrices
+
+
+class AntiDiagonalHandler(DiagonalHandlerAbstract):
+    def __init__(
+        self,
+        cfg: "ModelConfig",
+    ):
+        super().__init__(cfg)
+        self.diagonal_generator = self._init_model()
+
+    def forward(self, weight_params: Tensor, logits: Tensor) -> Tensor:
+        diagonal_matrices = self._compute_diagonal_matrix(logits)
         anti_diagonal_matrix = diagonal_matrices.flip(dims=[2])
         return weight_params + anti_diagonal_matrix
 
 
-class DiagonalAndAntiDiagonalHandler(DefaultDiagonalHandler):
+class DiagonalAndAntiDiagonalHandler(DiagonalHandlerAbstract):
     def __init__(
         self,
         cfg: "ModelConfig",
@@ -96,6 +106,6 @@ class DiagonalAndAntiDiagonalHandler(DefaultDiagonalHandler):
         self.diagonal_generator = DiagonalHandler(cfg)
         self.anti_diagonal_generator = AntiDiagonalHandler(cfg)
 
-    def forward(self, logits: Tensor, weight_params: Tensor) -> Tensor:
-        weight_params = self.diagonal_generator(logits, weight_params)
-        return self.anti_diagonal_generator(logits, weight_params)
+    def forward(self, weight_params: Tensor, logits: Tensor) -> Tensor:
+        weight_params = self.diagonal_generator(weight_params, logits)
+        return self.anti_diagonal_generator(weight_params, logits)
