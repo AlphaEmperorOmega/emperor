cbde8950b11fa64aa2283ff8b3f93dc4ef9fa385
AlphaEmperorOmega <alpha.emperor.omega@gmail.com>
2025-12-16 15:04:35 +0100

    [UPD]: Added validator to AdaptiveMixtureConfig #90

--- Emperor/adaptive/utils/mixtures/base.py
+++ Emperor/adaptive/utils/mixtures/base.py
@@ -1,14 +1,16 @@
-from Emperor.base.utils import Module, ConfigBase
 from dataclasses import dataclass, field
+from Emperor.base.utils import Module, ConfigBase
+from Emperor.adaptive.utils.mixtures._validator import _AdaptiveMixtureBaseValidator
 
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from Emperor.config import ModelConfig
+    from Emperor.experts.utils.layers import MixtureOfExpertsConfig
 
 
 @dataclass
-class MixtureConfig(ConfigBase):
+class AdaptiveMixtureConfig(ConfigBase):
     input_dim: int | None = field(
         default=None,
         metadata={"help": "Mixture model input dimension"},
@@ -17,10 +19,6 @@ class MixtureConfig(ConfigBase):
         default=None,
         metadata={"help": "Mixture model output dimension"},
     )
-    # depth_dim: int | None = field(
-    #     default=None,
-    #     metadata={"help": "Mixture model depth dimension"},
-    # )
     top_k: int | None = field(
         default=None,
         metadata={
@@ -37,31 +35,26 @@ class MixtureConfig(ConfigBase):
             "help": "When `True` the sepected parameters will be multiplied by their probs"
         },
     )
+    mixture_of_experts_config: "MixtureOfExpertsConfig | None" = field(
+        default=None,
+        metadata={"help": ""},
+    )
 
 
-class MixtureBase(Module):
+class AdaptiveMixtureBase(Module):
     def __init__(
         self,
-        cfg: "MixtureConfig | ModelConfig",
-        overrides: "MixtureConfig | None" = None,
+        cfg: "AdaptiveMixtureConfig | ModelConfig",
+        overrides: "AdaptiveMixtureConfig | None" = None,
     ):
         super().__init__()
         config = getattr(cfg, "mixture_model_config", cfg)
-        self.cfg: "MixtureConfig" = self._overwrite_config(config, overrides)
+        self.cfg: "AdaptiveMixtureConfig" = self._overwrite_config(config, overrides)
 
         self.input_dim = self.cfg.input_dim
         self.output_dim = self.cfg.output_dim
-        # self.depth_dim = self.cfg.depth_dim
         self.top_k = self.cfg.top_k
         self.num_experts = self.cfg.num_experts
         self.weighted_parameters_flag = self.cfg.weighted_parameters_flag
-        self.__validate_inputs()
-
-    def __validate_inputs(self) -> None:
-        # assert self.depth_dim == self.num_experts, (
-        #     "The `depth_dim` needs to be equal with `num_experts` since this is the dimension the router creates a distribution over."
-        # )
-        if self.num_experts == self.top_k:
-            assert self.weighted_parameters_flag is True, (
-                "If `full_mixture` is performed the `weighted_parameters_flag` must be True. Because the `weight_bank` or `bias_bank` needs to be broadcasted across the batch."
-            )
+        self.mixture_of_experts_config = self.cfg.mixture_of_experts_config
+        self._validator = _AdaptiveMixtureBaseValidator(self)
