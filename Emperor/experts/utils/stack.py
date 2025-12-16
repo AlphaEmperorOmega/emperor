0837b369d549968830560b90d9efab93c1b96d34
AlphaEmperorOmega <alpha.emperor.omega@gmail.com>
2025-12-16 11:43:55 +0100

    [UPD]: Added method to reset properties

--- Emperor/experts/utils/stack.py
+++ Emperor/experts/utils/stack.py
@@ -4,7 +4,10 @@ from Emperor.base.layer import Layer, LayerStack, LayerStackConfig
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
+    from Emperor.base.utils import Module
     from Emperor.config import ModelConfig
+    from Emperor.base.enums import ActivationOptions
+    from Emperor.base.enums import LayerNormPositionOptions
 
 
 class MixtureOfExpertsStack(LayerStack):
@@ -45,29 +48,47 @@ class MixtureOfExpertsLayer(Layer):
             layer_norm_position,
         )
 
-        self.total_loss = None
         self.probabilities = None
         self.indices = None
+        self.total_loss = None
 
-    def _handle_model_input(self, model_inputs: dict) -> Tensor:
+    def __reset_properties(self) -> None:
+        self.total_loss = None
+
+    def _handle_model_input(self, model_inputs: dict | Tensor) -> Tensor:
+        if isinstance(model_inputs, Tensor):
+            return model_inputs
+        if isinstance(model_inputs, tuple):
+            model_inputs, total_loss = model_inputs
+            if self.total_loss is None:
+                self.total_loss = total_loss
+            else:
+                self.total_loss = self.total_loss + total_loss
+            return model_inputs
+        input_batch = model_inputs["input_batch"]
         self.probabilities = model_inputs["probabilities"]
         self.indices = model_inputs["indices"]
         self.total_loss = model_inputs["loss"]
-        return model_inputs["input_batch"]
+        return input_batch
 
     def _handle_model_processing(self, model_input: Tensor) -> Tensor:
         model_output, total_loss = self.model(
             model_input, self.probabilities, self.indices
         )
-        self.total_loss += total_loss
+        if self.total_loss is None:
+            self.total_loss = total_loss
+        else:
+            self.total_loss = self.total_loss + total_loss
         return model_output
 
     def _handle_model_output(self, output: Tensor) -> tuple | dict:
-        if self.last_layer_flag:
-            return output, self.total_loss
+        total_loss = self.total_loss
+        self.__reset_properties()
+        if self.last_layer_flag or self.probabilities is None:
+            return output, total_loss
         return {
             "input_batch": output,
             "probabilities": self.probabilities,
             "indices": self.indices,
-            "loss": self.total_loss,
+            "loss": total_loss,
         }
