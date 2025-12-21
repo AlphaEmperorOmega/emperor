4cfa5d1a363a11ac69d69f5fa6925eaf647e7bf4
AlphaEmperorOmega <alpha.emperor.omega@gmail.com>
2025-12-21 19:37:01 +0100

    [UPD]: Added enumeration options for adaptive components

---
+++ Emperor/adaptive/utils/mixtures/options.py
@@ -0,0 +1,14 @@
+from Emperor.base.enums import BaseOptions
+
+
+class AdaptiveWeightOptions(BaseOptions):
+    VECTOR = 1
+    MATRIX = 2
+    GENERATOR = 3
+
+
+class AdaptiveBiasOptions(BaseOptions):
+    DISABLED = 0
+    VECTOR = 1
+    MATRIX = 2
+    GENERATOR = 3
