96ef54dcaa563385c51151a5633f8307f55adff5
AlphaEmperorOmega <alpha.emperor.omega@gmail.com>
2026-01-04 19:29:30 +0100

    [UPD]: Added AdaptiveParameterLayer handler to build the sampler models

--- Emperor/adaptive/utils/layers.py
+++ Emperor/adaptive/utils/layers.py
@@ -4,16 +4,11 @@ from enum import Enum
 from torch import Tensor
 from dataclasses import dataclass, field
 from Emperor.base.decorators import timer
-from Emperor.sampler.model import SamplerModel
 from Emperor.base.utils import ConfigBase, Module
 from Emperor.sampler.utils.samplers import SamplerConfig
-from Emperor.adaptive.utils.routers import VectorRouterModel
-from Emperor.sampler.utils.routers import RouterConfig, RouterModel
+from Emperor.sampler.utils.routers import RouterConfig
 from Emperor.adaptive.utils.mixtures.base import AdaptiveMixtureBase
-from Emperor.adaptive.utils._validator import (
-    _AdaptiveParameterLayerValidator,
-    _AdaptiveParameterHandlerValidator,
-)
+from Emperor.adaptive.utils._validator import _AdaptiveParameterLayerValidator
 from Emperor.adaptive.utils.mixtures.selectors import (
     AdaptiveWeightSelector,
     AdaptiveBiasSelector,
@@ -25,6 +20,11 @@ from Emperor.behaviours.model import (
 from Emperor.adaptive.utils.mixtures.options import (
     AdaptiveBiasOptions,
     AdaptiveWeightOptions,
+)
+from Emperor.adaptive.utils.handlers import (
+    GeneratorParameterHandler,
+    MatrixParameterHandler,
+    VectorParameterHandler,
 )
 
 from typing import TYPE_CHECKING
@@ -84,111 +84,6 @@ class AdaptiveParameterLayerConfig(ConfigBase):
         default=None,
         metadata={"help": ""},
     )
-
-
-class ParameterHanlderBase(Module):
-    def __init__(
-        self,
-        cfg: "AdaptiveParameterLayerConfig | ModelConfig",
-    ):
-        super().__init__()
-        self.cfg = cfg
-        self.input_dim = self.cfg.input_dim
-        self.output_dim = self.cfg.output_dim
-        self.adaptive_weight_option = self.cfg.adaptive_weight_option
-        self.adaptive_bias_option = self.cfg.adaptive_bias_option
-        self.init_sampler_model_option = self.cfg.init_sampler_model_option
-        self.router_config = self.cfg.router_config
-        self.sampler_config = self.cfg.sampler_config
-        self.validator = _AdaptiveParameterHandlerValidator(self)
-
-    def build_sampler_models(
-        self,
-    ) -> tuple[RouterModel | None, RouterModel | None, SamplerModel | None]:
-        shared_option = AdaptiveRouterOptions.SHARED_ROUTER
-        if self.init_sampler_model_option == shared_option:
-            return self._init_shared_sampler()
-        return self._init_independent_sampler()
-
-    def _init_shared_sampler(self):
-        raise NotImplementedError(
-            "The method `_init_shared_sampler` must be implemented in the child class."
-        )
-
-    def _init_independent_sampler(self):
-        raise NotImplementedError(
-            "The method `_init_independent_sampler` must be implemented in the child class."
-        )
-
-    def _init_bias_router_model(self):
-        if self.adaptive_bias_option == AdaptiveBiasOptions.GENERATOR:
-            return None
-        return RouterModel(self.router_config)
-
-
-class VectorParameterHandler(ParameterHanlderBase):
-    def __init__(
-        self,
-        cfg: "AdaptiveParameterLayerConfig | ModelConfig",
-    ):
-        super().__init__(cfg)
-
-    def _init_shared_sampler(self) -> None:
-        self.validator.ensure_shared_sampler_is_disabled()
-
-    def _init_independent_sampler(
-        self,
-    ) -> tuple[VectorRouterModel, RouterModel | None, SamplerModel]:
-        self.validator.ensure_indepentent_router_for_vector_option()
-        weight_router = VectorRouterModel(self.router_config)
-        bias_router = self._init_bias_router_model()
-        sampler = SamplerModel(self.sampler_config)
-        return weight_router, bias_router, sampler
-
-
-class MatrixParameterHandler(ParameterHanlderBase):
-    def __init__(
-        self,
-        cfg: "AdaptiveParameterLayerConfig | ModelConfig",
-    ):
-        super().__init__(cfg)
-
-    def _init_shared_sampler(self) -> tuple[RouterModel, None, SamplerModel]:
-        self.validator.ensure_router_and_sampler_configs_exist()
-        router = RouterModel(self.router_config)
-        sampler = SamplerModel(self.sampler_config)
-        return router, None, sampler
-
-    def _init_independent_sampler(
-        self,
-    ) -> tuple[RouterModel, RouterModel | None, SamplerModel]:
-        self.validator.ensure_router_and_sampler_configs_exist()
-        weights_router = RouterModel(self.router_config)
-        bias_router = self._init_bias_router_model()
-        sampler = SamplerModel(self.sampler_config)
-        return weights_router, bias_router, sampler
-
-
-class GeneratorParameterHandler(ParameterHanlderBase):
-    def __init__(
-        self,
-        cfg: "AdaptiveParameterLayerConfig | ModelConfig",
-    ):
-        super().__init__(cfg)
-
-    def _init_shared_sampler(self) -> tuple[RouterModel, None, SamplerModel]:
-        self.validator.ensure_router_and_sampler_configs_exist()
-        router = RouterModel(self.router_config)
-        sampler = SamplerModel(self.sampler_config)
-        return router, None, sampler
-
-    def _init_independent_sampler(
-        self,
-    ) -> tuple[RouterModel | None, RouterModel | None, SamplerModel | None]:
-        weights_router = None
-        bias_router = self._init_bias_router_model()
-        sampler = SamplerModel(self.sampler_config)
-        return weights_router, bias_router, sampler
 
 
 class AdaptiveParameterLayer(Module):
