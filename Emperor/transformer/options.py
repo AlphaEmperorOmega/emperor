b9fb478e2674177eb55c9d343d6ba32d229736b9
AlphaEmperorOmega <alpha.emperor.omega@gmail.com>
2026-01-18 00:57:07 +0100

    [UPD]: Updated transformer options with propper class names

--- Emperor/transformer/options.py
+++ Emperor/transformer/options.py
@@ -1,12 +1,12 @@
 from Emperor.base.enums import BaseOptions
+from Emperor.transformer.utils.models import Transformer
 from Emperor.transformer.utils.stack import (
-    Transformer,
-    TransformerDecoder,
-    TransformerEncoder,
+    TransformerDecoderStack,
+    TransformerEncoderStack,
 )
 
 
 class TransformerOptions(BaseOptions):
     DEFAULT = Transformer
-    ENCODER = TransformerEncoder
-    DECODER = TransformerDecoder
+    ENCODER = TransformerEncoderStack
+    DECODER = TransformerDecoderStack
