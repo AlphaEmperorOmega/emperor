2c5deef4492176f26bb5c6379f3188795421e4ce
AlphaEmperorOmega <alpha.emperor.omega@gmail.com>
2026-01-18 00:53:32 +0100

    [UPD]: Updated the test.py with new tests

--- test.py
+++ test.py
@@ -1,6 +1,11 @@
-from Emperor.adaptive.experiments import AdaptiveParameterExperiments
-from Emperor.experts.experiments import MixtureOfExpertsExperiments
+from Emperor.experiments.models.bert_vit_transformer import (
+    BERTVITExperiment,
+    BERTVITExperimentOptions,
+)
 from Emperor.linears.experiments import LinearsExperiments
+from Emperor.transformer.experiments import TransformerExperiments
+from Emperor.experts.experiments import MixtureOfExpertsExperiments
+from Emperor.adaptive.experiments import AdaptiveParameterExperiments
 
 
 if __name__ == "__main__":
@@ -11,4 +16,7 @@ if __name__ == "__main__":
     # LinearsExperiments(**options).train_dynamic_model()
     # LinearsExperiments(**options).test_all_linear_types()
     # MixtureOfExpertsExperiments(**options).test_all_types()
-    AdaptiveParameterExperiments(**options).test_all_types()
+    # AdaptiveParameterExperiments(**options).test_all_types()
+    # TransformerExperiments(**options).train_transformer_BERT_VIT_model()
+
+    BERTVITExperiment(**options).train_model(BERTVITExperimentOptions.ADAPTIVE_VIT)
