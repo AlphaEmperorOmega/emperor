0fe91daf24939fa10e7333278e1725108dc6be5f
AlphaEmperorOmega <alpha.emperor.omega@gmail.com>
2026-01-22 22:20:38 +0100

    [UPD]: Added MixtureOfMultiHeadAttention projector

--- Emperor/attention/utils/handlers/projector.py
+++ Emperor/attention/utils/handlers/projector.py
@@ -2,11 +2,15 @@ import torch.nn as nn
 
 from torch.types import Tensor
 from Emperor.base.utils import Module
+from torch.nn.modules import Sequential
 from Emperor.base.layer import Layer, LayerStackConfig
-from Emperor.attention.utils.handlers._validator import (
+from Emperor.experts.utils.model import MixtureOfExpertsModel
+from Emperor.attention.utils.handlers.validators._projector import (
     IndependentProjectorValidator,
+    MixtureOfAttentionHeadsProjectorValidator,
     SelfAttentionProjectorValidator,
 )
+
 
 from typing import TYPE_CHECKING
 
@@ -14,7 +18,7 @@ if TYPE_CHECKING:
     from Emperor.attention.utils.layer import MultiHeadAttentionConfig
 
 
-class ProjectorSelector(Module):
+class ProjectorBuilder(Module):
     def __init__(
         self,
         cfg: "MultiHeadAttentionConfig",
@@ -25,20 +29,24 @@ class ProjectorSelector(Module):
         self.embedding_dim = self.cfg.embedding_dim
         self.query_key_projection_dim = self.cfg.query_key_projection_dim
         self.value_projection_dim = self.cfg.value_projection_dim
-        self.is_self_attention_projector_flag = (
-            self.cfg.is_self_attention_projector_flag
-        )
+        self.projector_option = self.cfg.projector_option
 
     def build_model(self):
-        if self.__should_use_self_attention_projector():
-            return SelfAttentionProjector(self.cfg)
-        return IndependentProjector(self.cfg)
+        from Emperor.attention.utils.enums import ProjectorOptions
+
+        match self.projector_option:
+            case ProjectorOptions.SELF_ATTENTION:
+                self.__should_use_self_attention_projector()
+                return SelfAttentionProjector(self.cfg)
+            case ProjectorOptions.INDEPENDENT:
+                return IndependentProjector(self.cfg)
+            case ProjectorOptions.MIXTURE_OF_ATTENTION_HEADS:
+                return IndependentProjector(self.cfg)
 
     def __should_use_self_attention_projector(self):
-        is_self_attention = self.is_self_attention_projector_flag
         are_qk_dims_same = self.embedding_dim == self.query_key_projection_dim
         are_qv_dims_same = self.embedding_dim == self.value_projection_dim
-        return is_self_attention and (are_qk_dims_same and are_qv_dims_same)
+        return are_qk_dims_same and are_qv_dims_same
 
 
 class ProjectorBase(Module):
@@ -56,9 +64,9 @@ class ProjectorBase(Module):
         self.query_key_projection_dim = self.cfg.query_key_projection_dim
         self.value_projection_dim = self.cfg.value_projection_dim
         self.return_attention_weights_flag = self.cfg.return_attention_weights_flag
-        self.is_self_attention_projector_flag = (
-            self.cfg.is_self_attention_projector_flag
-        )
+        self.projector_option = self.cfg.projector_option
+        self.experts_config = self.cfg.experts_config
+        self.use_kv_expert_models_flag = self.cfg.use_kv_expert_models_flag
         self.__resolve_kv_dimensions()
         self.output_model = self._build_output_model()
 
@@ -113,7 +121,7 @@ class SelfAttentionProjector(ProjectorBase):
         self.qkv_model = self._create_model(self.embedding_dim, self.embedding_dim * 3)
         self.validator = SelfAttentionProjectorValidator(self)
 
-    def _build_output_model(self) -> tuple:
+    def _build_output_model(self) -> Layer | Sequential:
         return self._create_model(self.embedding_dim, self.embedding_dim)
 
     def compute_qkv_projections(
@@ -173,3 +181,82 @@ class IndependentProjector(ProjectorBase):
         value_projections = self._compute_projection(value, self.value_model)
 
         return query_projections, key_projections, value_projections
+
+
+class MixtureOfAttentionHeadsProjector(ProjectorBase):
+    def __init__(
+        self,
+        cfg: "MultiHeadAttentionConfig",
+        overrides: "MultiHeadAttentionConfig | None" = None,
+    ):
+        super().__init__(cfg, overrides)
+
+        self.validator = MixtureOfAttentionHeadsProjectorValidator(self)
+
+        qk_dims = (self.embedding_dim, self.query_key_projection_dim)
+        v_dims = (self.embedding_dim, self.value_projection_dim)
+        self.query_model = self._create_experts_model(*qk_dims)
+        self.key_model = self._create_kv_model(*qk_dims)
+        self.value_model = self._create_kv_model(*v_dims)
+        self.top_k = self.query_model.get_top_k()
+
+    def _create_kv_model(self, input_dim: int, output_dim: int):
+        if self.use_kv_expert_models_flag:
+            return self._create_experts_model(input_dim, output_dim)
+        return self._create_model(input_dim, output_dim)
+
+    def _build_output_model(self) -> MixtureOfExpertsModel:
+        return self._create_experts_model(self.value_projection_dim, self.embedding_dim)
+
+    def _create_experts_model(
+        self, input_dim: int, output_dim: int
+    ) -> MixtureOfExpertsModel:
+        overrides = LayerStackConfig(
+            input_dim=input_dim,
+            output_dim=output_dim,
+        )
+        return MixtureOfExpertsModel(self.experts_config, overrides)
+
+    def compute_qkv_projections(
+        self,
+        query: Tensor,
+        key: Tensor,
+        value: Tensor,
+    ) -> tuple[Tensor, Tensor, Tensor]:
+        self.validator.ensure_attention_weights_returned_for_self_attention_only()
+        self.validator.ensure_propper_kv_shapes_for_independent_projector(key, value)
+        query_projections = self._compute_q_projection(query, self.query_model)
+        key_projections = self._compute_kv_projection(key, self.key_model)
+        value_projections = self._compute_kv_projection(value, self.value_model)
+
+        return query_projections, key_projections, value_projections
+
+    def _compute_q_projection(self, tensor: Tensor, model: nn.Module) -> Tensor:
+        sequence_length, batch_size, _ = tensor.shape
+        projection = self._compute_projection(tensor, model)
+        return projection.view(sequence_length, batch_size, self.top_k, -1)
+
+    def _compute_kv_projection(self, tensor: Tensor, model: nn.Module) -> Tensor:
+        sequence_length, batch_size, _ = tensor.shape
+        projection = self._compute_projection(tensor, model)
+        if self.use_kv_expert_models_flag:
+            return projection.view(sequence_length, batch_size, self.top_k, -1)
+        return projection.view(sequence_length, batch_size, -1)
+
+    def _compute_projection(self, tensor: Tensor, model: nn.Module) -> Tensor:
+        _, _, embedding_dim = tensor.shape
+        if not tensor.is_contiguous():
+            tensor = tensor.contiguous()
+
+        tensor_reshaped = tensor.view(-1, embedding_dim)
+        projection = model(tensor_reshaped)
+
+        # TODO: In the future return those to make use of them on higher
+        # level classes
+        if isinstance(projection, tuple):
+            if len(projection) == 2:
+                projection, loss = projection
+            elif len(projection) == 3:
+                projection, skip_mask, loss = projection
+
+        return projection
