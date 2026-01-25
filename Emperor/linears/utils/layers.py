67b2a536b6a1ef078d4a3b0d453d0eb34af2b1f6
AlphaEmperorOmega <alpha.emperor.omega@gmail.com>
2026-01-25 10:27:31 +0100

    [UPD]: Updated linear layers type hints

--- Emperor/linears/utils/layers.py
+++ Emperor/linears/utils/layers.py
@@ -53,16 +53,19 @@ class LinearBase(Module):
         super().__init__()
         config = getattr(cfg, "linear_layer_config", cfg)
         self.cfg: "LinearLayerConfig" = self._overwrite_config(config, overrides)
-        self.main_cfg = self._resolve_main_config(self.cfg, cfg)
-        self.input_dim = self.cfg.input_dim
-        self.output_dim = self.cfg.output_dim
-        self.bias_flag = self.cfg.bias_flag
-        self.data_monitor: "DataMonitor" = self.construct(self.cfg.data_monitor)
-        self.parameter_monitor: "ParameterMonitor" = self.construct(
+        self.main_cfg: "AdaptiveParameterBehaviourConfig" = self._resolve_main_config(
+            self.cfg, cfg
+        )
+        self.input_dim: int = self.cfg.input_dim
+        self.output_dim: int = self.cfg.output_dim
+        self.bias_flag: bool = self.cfg.bias_flag
+        self.data_monitor: DataMonitor = self.construct(self.cfg.data_monitor)
+        self.weight_params, self.bias_params = self._init_parameters()
+        self.parameter_monitor: ParameterMonitor = self.construct(
             self.cfg.parameter_monitor
         )
 
-    def _init_parameters(self):
+    def _init_parameters(self) -> tuple[Parameter, Parameter | None]:
         weight_params = self.__init_weight_parameters()
         bias_params = self.__init_bias_parameters()
         return weight_params, bias_params
@@ -77,13 +80,13 @@ class LinearBase(Module):
         bias_shape = (self.output_dim,)
         return self._init_parameter_bank(bias_shape, nn.init.zeros_)
 
-    def _update_data_monitor(self, input_batch: Tensor, output_batch: Tensor) -> None:
-        self.data_monitor and self.data_monitor.update(input_batch, output_batch)
+    def _update_data_monitor(self, X: Tensor, output: Tensor) -> None:
+        if self.data_monitor:
+            self.data_monitor.update(X, output)
 
     def _update_parameter_monitor(self) -> None:
-        self.data_monitor and self.data_monitor.update(
-            self.weight_params, self.bias_params
-        )
+        if self.parameter_monitor:
+            self.parameter_monitor.update(self.weight_params, self.bias_params)
 
 
 class LinearLayer(LinearBase):
@@ -93,11 +96,10 @@ class LinearLayer(LinearBase):
         overrides: "LinearLayerConfig | None" = None,
     ):
         super().__init__(cfg, overrides)
-        self.weight_params, self.bias_params = self._init_parameters()
 
-    def forward(self, input_batch: Tensor) -> Tensor:
-        output = F.linear(input_batch, self.weight_params.T, self.bias_params)
-        self._update_data_monitor(input_batch, output)
+    def forward(self, X: Tensor) -> Tensor:
+        output = F.linear(X, self.weight_params.T, self.bias_params)
+        self._update_data_monitor(X, output)
         self._update_parameter_monitor()
         return output
 
@@ -119,35 +121,34 @@ class AdaptiveLinearLayer(LinearBase):
         )
         return AdaptiveParameterBehaviour(self.main_cfg, overrides)
 
-    def forward(self, input: Tensor) -> Tensor:
-        output = self.adaptive_behaviour.compute_adaptive_parameters(
+    def forward(self, X: Tensor) -> Tensor:
+        return self.adaptive_behaviour.compute_adaptive_parameters(
             self._compute_affine_transformation_callback,
             self.weight_params,
             self.bias_params,
-            input,
+            X,
         )
-        return output
 
     def _compute_affine_transformation_callback(
-        self, weights: Tensor, bias: Tensor | None, input: Tensor
+        self, weights: Tensor, bias: Tensor | None, X: Tensor
     ) -> Tensor:
-        output = self.__compute_linear_transformation(input, weights)
+        output = self.__compute_linear_transformation(X, weights)
         return self.__add_bias_parameters(output, bias)
 
     def __compute_linear_transformation(
         self,
-        logits: Tensor,
+        X: Tensor,
         dynamic_diagonal_weights: Tensor,
     ) -> Tensor:
         if dynamic_diagonal_weights.dim() == 3:
-            return torch.einsum("ij,ijk->ik", logits, dynamic_diagonal_weights)
-        return torch.matmul(logits, dynamic_diagonal_weights)
+            return torch.einsum("ij,ijk->ik", X, dynamic_diagonal_weights)
+        return torch.matmul(X, dynamic_diagonal_weights)
 
     def __add_bias_parameters(
         self,
-        linear_transform: Tensor,
+        X: Tensor,
         bias_params: Tensor | None = None,
     ) -> Tensor:
         if bias_params is not None:
-            return linear_transform + bias_params
-        return linear_transform
+            return X + bias_params
+        return X
