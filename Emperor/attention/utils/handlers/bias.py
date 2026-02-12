8b81c4daca85b6ad0058f01c34741d3d86ee495c
AlphaEmperorOmega <alpha.emperor.omega@gmail.com>
2026-02-12 10:11:23 +0100

    [UPD]: Bias for qk dim and v dim

--- Emperor/attention/utils/handlers/bias.py
+++ Emperor/attention/utils/handlers/bias.py
@@ -15,14 +15,28 @@ class KeyValueBias(Module):
         self.cfg = cfg
         self.batch_size = self.cfg.batch_size
         self.embedding_dim = self.cfg.embedding_dim
+        self.query_key_projection_dim = self.cfg.query_key_projection_dim
+        self.value_projection_dim = self.cfg.value_projection_dim
         self.add_key_value_bias_flag = self.cfg.add_key_value_bias_flag
+        self.__resolve_kv_dimensions()
+
         self.key_bias_vector, self.value_bias_vector = self.__build_kv_bias_vectors()
+
+    def __resolve_kv_dimensions(self) -> None:
+        is_qk_dim_zero = self.query_key_projection_dim == 0
+        is_v_dim_zero = self.value_projection_dim == 0
+        self.query_key_projection_dim = (
+            self.embedding_dim if is_qk_dim_zero else self.query_key_projection_dim
+        )
+        self.value_projection_dim = (
+            self.embedding_dim if is_v_dim_zero else self.value_projection_dim
+        )
 
     def __build_kv_bias_vectors(self):
         if not self.add_key_value_bias_flag:
             return None, None
-        bias_k = self._init_parameter_bank((1, 1, self.embedding_dim))
-        bias_v = self._init_parameter_bank((1, 1, self.embedding_dim))
+        bias_k = self._init_parameter_bank((1, 1, self.query_key_projection_dim))
+        bias_v = self._init_parameter_bank((1, 1, self.value_projection_dim))
         return bias_k, bias_v
 
     def add_kv_learnable_bias_vectors(
