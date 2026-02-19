f1b14c4b6e857fc92baf8905be598638c8479ebf
AlphaEmperorOmega <alpha.emperor.omega@gmail.com>
2026-02-19 11:35:30 +0100

    [UPD]: Removed ability to close window when error occurs in run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -3,12 +3,11 @@
 DOCS_DIR="docs"
 
 if [ -n "$1" ]; then
-    TEST_PATH="$DOCS_DIR/test_$1"
-    if [ ! -f "$TEST_PATH" ]; then
-        echo "Error: $TEST_PATH not found"
-        exit 1
-    fi
-    python3 -m unittest -f "$TEST_PATH"
+  TEST_PATH="$DOCS_DIR/test_$1"
+  if [ ! -f "$TEST_PATH" ]; then
+    echo "Error: $TEST_PATH not found"
+  fi
+  python3 -m unittest -f "$TEST_PATH"
 else
-    python3 -m unittest discover -f "$DOCS_DIR"
+  python3 -m unittest discover -f "$DOCS_DIR"
 fi
