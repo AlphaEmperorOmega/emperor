72b2e2817c55ea29f49652d8fba495c78196bd11
AlphaEmperorOmega <alpha.emperor.omega@gmail.com>
2026-02-26 09:26:03 +0100

    [UPD]: Updated datasets

--- Emperor/datasets/image/cifar_100.py
+++ Emperor/datasets/image/cifar_100.py
@@ -8,17 +8,18 @@ from torchvision.transforms.transforms import Compose
 
 
 class Cifar100(DataModule):
+    default_width: int = 32
+    default_height: int = 32
+    num_classes: int = 100
+    num_channels: int = 3
+    flattened_input_dim: int = default_width * default_height * num_channels
+
     def __init__(
         self,
         batch_size,
         resize=(32, 32),
-        test_dataset_flag=False,
-        test_dataset_num_samples=64,
     ):
-        super().__init__(
-            test_dataset_flag=test_dataset_flag,
-            test_dataset_num_samples=test_dataset_num_samples,
-        )
+        super().__init__()
         self.batch_size = batch_size
         self.resize = resize
 
@@ -26,19 +27,24 @@ class Cifar100(DataModule):
         datasets.CIFAR100(root=self.root, train=True, download=True)
         datasets.CIFAR100(root=self.root, train=False, download=True)
 
-    def setup(self, stage: str) -> None:
-        train = datasets.CIFAR100(
+    def _setup_fit(self) -> None:
+        self.train = datasets.CIFAR100(
             root=self.root,
             train=True,
             transform=self.__get_train_transforms(),
         )
-        val = datasets.CIFAR100(
+        self.val = datasets.CIFAR100(
             root=self.root,
             train=False,
             transform=self.__get_test_transforms(),
         )
-        self.train = self.get_dataset(train)
-        self.val = self.get_dataset(val)
+
+    def _setup_validate(self) -> None:
+        self.val = datasets.CIFAR100(
+            root=self.root,
+            train=False,
+            transform=self.__get_test_transforms(),
+        )
 
     def __get_train_transforms(self) -> Compose:
         return transforms.Compose(
@@ -67,22 +73,106 @@ class Cifar100(DataModule):
 
     def _text_labels(self, indices) -> list:
         labels = [
-            "apple", "aquarium_fish", "baby", "bear", "beaver", "bed", "bee",
-            "beetle", "bicycle", "bottle", "bowl", "boy", "bridge", "bus",
-            "butterfly", "camel", "can", "castle", "caterpillar", "cattle",
-            "chair", "chimpanzee", "clock", "cloud", "cockroach", "couch",
-            "crab", "crocodile", "cup", "dinosaur", "dolphin", "elephant",
-            "flatfish", "forest", "fox", "girl", "hamster", "house",
-            "kangaroo", "keyboard", "lamp", "lawn_mower", "leopard", "lion",
-            "lizard", "lobster", "man", "maple_tree", "motorcycle", "mountain",
-            "mouse", "mushroom", "oak_tree", "orange", "orchid", "otter",
-            "palm_tree", "pear", "pickup_truck", "pine_tree", "plain", "plate",
-            "poppy", "porcupine", "possum", "rabbit", "raccoon", "ray", "road",
-            "rocket", "rose", "sea", "seal", "shark", "shrew", "skunk",
-            "skyscraper", "snail", "snake", "spider", "squirrel", "streetcar",
-            "sunflower", "sweet_pepper", "table", "tank", "telephone",
-            "television", "tiger", "tractor", "train", "trout", "tulip",
-            "turtle", "wardrobe", "whale", "willow_tree", "wolf", "woman", "worm",
+            "apple",
+            "aquarium_fish",
+            "baby",
+            "bear",
+            "beaver",
+            "bed",
+            "bee",
+            "beetle",
+            "bicycle",
+            "bottle",
+            "bowl",
+            "boy",
+            "bridge",
+            "bus",
+            "butterfly",
+            "camel",
+            "can",
+            "castle",
+            "caterpillar",
+            "cattle",
+            "chair",
+            "chimpanzee",
+            "clock",
+            "cloud",
+            "cockroach",
+            "couch",
+            "crab",
+            "crocodile",
+            "cup",
+            "dinosaur",
+            "dolphin",
+            "elephant",
+            "flatfish",
+            "forest",
+            "fox",
+            "girl",
+            "hamster",
+            "house",
+            "kangaroo",
+            "keyboard",
+            "lamp",
+            "lawn_mower",
+            "leopard",
+            "lion",
+            "lizard",
+            "lobster",
+            "man",
+            "maple_tree",
+            "motorcycle",
+            "mountain",
+            "mouse",
+            "mushroom",
+            "oak_tree",
+            "orange",
+            "orchid",
+            "otter",
+            "palm_tree",
+            "pear",
+            "pickup_truck",
+            "pine_tree",
+            "plain",
+            "plate",
+            "poppy",
+            "porcupine",
+            "possum",
+            "rabbit",
+            "raccoon",
+            "ray",
+            "road",
+            "rocket",
+            "rose",
+            "sea",
+            "seal",
+            "shark",
+            "shrew",
+            "skunk",
+            "skyscraper",
+            "snail",
+            "snake",
+            "spider",
+            "squirrel",
+            "streetcar",
+            "sunflower",
+            "sweet_pepper",
+            "table",
+            "tank",
+            "telephone",
+            "television",
+            "tiger",
+            "tractor",
+            "train",
+            "trout",
+            "tulip",
+            "turtle",
+            "wardrobe",
+            "whale",
+            "willow_tree",
+            "wolf",
+            "woman",
+            "worm",
         ]
         return [labels[int(i)] for i in indices]
 
--- Emperor/datasets/image/fashion_mnist.py
+++ Emperor/datasets/image/fashion_mnist.py
@@ -8,17 +8,18 @@ from Emperor.base.utils import DataModule
 
 
 class FashionMNIST(DataModule):
+    default_width: int = 28
+    default_height: int = 28
+    num_classes: int = 10
+    num_channels: int = 1
+    flattened_input_dim: int = default_width * default_height * num_channels
+
     def __init__(
         self,
         batch_size,
         resize=(28, 28),
-        test_dataset_flag=False,
-        test_dataset_num_samples=64,
     ):
-        super().__init__(
-            test_dataset_flag=test_dataset_flag,
-            test_dataset_num_samples=test_dataset_num_samples,
-        )
+        super().__init__()
         self.batch_size = batch_size
         self.resize = resize
 
@@ -26,19 +27,24 @@ class FashionMNIST(DataModule):
         datasets.FashionMNIST(root=self.root, train=True, download=True)
         datasets.FashionMNIST(root=self.root, train=False, download=True)
 
-    def setup(self, stage: str) -> None:
-        train = datasets.FashionMNIST(
+    def _setup_fit(self) -> None:
+        self.train = datasets.FashionMNIST(
             root=self.root,
             train=True,
             transform=self.__get_train_transforms(),
         )
-        val = datasets.FashionMNIST(
+        self.val = datasets.FashionMNIST(
             root=self.root,
             train=False,
             transform=self.__get_test_transforms(),
         )
-        self.train = self.get_dataset(train)
-        self.val = self.get_dataset(val)
+
+    def _setup_validate(self) -> None:
+        self.val = datasets.FashionMNIST(
+            root=self.root,
+            train=False,
+            transform=self.__get_test_transforms(),
+        )
 
     def __get_train_transforms(self) -> Compose:
         return transforms.Compose(
@@ -60,8 +66,16 @@ class FashionMNIST(DataModule):
 
     def _text_labels(self, indices) -> list:
         labels = [
-            "t-shirt", "trouser", "pullover", "dress", "coat",
-            "sandal", "shirt", "sneaker", "bag", "ankle boot",
+            "t-shirt",
+            "trouser",
+            "pullover",
+            "dress",
+            "coat",
+            "sandal",
+            "shirt",
+            "sneaker",
+            "bag",
+            "ankle boot",
         ]
         return [labels[int(i)] for i in indices]
 
