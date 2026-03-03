51c5bf2af6d21a92cea2f283e8e5f7255a1ed8d6
AlphaEmperorOmega <alpha.emperor.omega@gmail.com>
2026-03-03 10:23:24 +0100

    [UPD]: Update all __main__.py files to use resolve_experiment_mode

--- models/parametric_matrix/__main__.py
+++ models/parametric_matrix/__main__.py
@@ -1,9 +1,9 @@
-from models.parser import get_experiment_parser
+from models.parser import get_experiment_parser, resolve_experiment_mode
 from models.parametric_matrix import Experiment, ExperimentOptions
 
 if __name__ == "__main__":
     parser = get_experiment_parser(ExperimentOptions.names())
     args = parser.parse_args()
-    config_option = ExperimentOptions.get_option(args.name)
+    config_option, num_samples = resolve_experiment_mode(args, ExperimentOptions)
     experiment = Experiment(config_option)
-    experiment.train_model(num_samples=args.num_samples, log_folder=args.log_folder)
+    experiment.train_model(num_samples=num_samples, log_folder=args.log_folder)
