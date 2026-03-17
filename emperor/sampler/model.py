5618e6b7af416c7d9076e3a4ab3b9aeca2447176
AlphaEmperorOmega <alpha.emperor.omega@gmail.com>
2026-03-17 23:22:35 +0100

    [UPD]: Add optional router support to SamplerModel

--- emperor/sampler/model.py
+++ emperor/sampler/model.py
@@ -1,5 +1,7 @@
 from torch import Tensor
 from emperor.base.utils import Module
+from emperor.sampler.utils.routers import RouterConfig, RouterModel
+from emperor.sampler.utils._validator import SamplerModelValidator
 from emperor.sampler.utils.samplers import (
     SamplerBase,
     SamplerConfig,
@@ -27,10 +29,13 @@ class SamplerModel(Module):
         config = getattr(cfg, "sampler_model_config", cfg)
         self.sampler_config: "SamplerConfig" = self._overwrite_config(config, overrides)
 
-        self.num_experts = self.sampler_config.num_experts
-        self.sampler_model = self._init_sampler_model()
+        self.num_experts: int = self.sampler_config.num_experts
+        self.router_config: "RouterConfig" = self.sampler_config.router_config
+        self.validator = SamplerModelValidator(self)
+        self.sampler_model = self.__init_sampler_model()
+        self.router = self.__maybe_init_router()
 
-    def _init_sampler_model(self) -> SamplerBase:
+    def __init_sampler_model(self) -> SamplerBase:
         if self.sampler_config.top_k == 1:
             return SamplerSparse(self.cfg, self.overrides)
         elif self.sampler_config.top_k == self.num_experts:
@@ -38,12 +43,23 @@ class SamplerModel(Module):
         else:
             return SamplerTopk(self.cfg, self.overrides)
 
+    def __maybe_init_router(self) -> RouterModel | None:
+        if self.router_config is None:
+            return None
+        return RouterModel(self.router_config)
+
     def sample_probabilities_and_indices(
         self,
         input_matrix: Tensor,
         skip_mask: Tensor | None = None,
     ) -> tuple[Tensor, Tensor | None, Tensor | None, Tensor]:
-        return self.sampler_model.get_probabilities_and_indices(input_matrix, skip_mask)
+        logits = self.__maybe_compute_routing(input_matrix)
+        return self.sampler_model.get_probabilities_and_indices(logits, skip_mask)
+
+    def __maybe_compute_routing(self, input_matrix: Tensor) -> Tensor:
+        if self.router is not None:
+            return self.router.compute_logit_scores(input_matrix)
+        return input_matrix
 
     def get_updated_skip_mask(self) -> Tensor | None:
         return self.sampler_model.updated_skip_mask
