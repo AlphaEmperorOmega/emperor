a309195c0a496d499ed4da17082b91c15f3cb078
AlphaEmperorOmega <alpha.emperor.omega@gmail.com>
2026-03-17 14:24:13 +0100

    [UPD]: Update linear model grid search config

--- models/linear/config.py
+++ models/linear/config.py
@@ -8,17 +8,17 @@ from emperor.datasets.image.classification.cifar_100 import Cifar100
 from emperor.datasets.image.classification.fashion_mnist import FashionMNIST
 
 # Global
-BATCH_SIZE: int = 64
+BATCH_SIZE: int = 128
 LEARNING_RATE: float = 1e-3
-NUM_EPOCHS: int = 1  # 10
+NUM_EPOCHS: int = 10
 DATASET_OPTIONS: list = [Mnist, FashionMNIST, Cifar10, Cifar100]
 
 # Trainer
-GRADIENT_CLIP_VAL: float = 0.0
+GRADIENT_CLIP_VAL: float = 1.0
 GRADIENT_CLIP_ALGORITHM: str = "norm"
 ACCUMULATE_GRAD_BATCHES: int = 1
 PRECISION: str = "32-true"
-DETERMINISTIC: bool = False
+DETERMINISTIC: bool = True
 BENCHMARK: bool = True
 MAX_STEPS: int = -1
 MAX_TIME: str | None = None
@@ -30,7 +30,7 @@ NUM_SANITY_VAL_STEPS: int = 2
 LOG_EVERY_N_STEPS: int = 50
 ENABLE_PROGRESS_BAR: bool = True
 PROFILER: str | None = None
-EARLY_STOPPING_PATIENCE: int = 0
+EARLY_STOPPING_PATIENCE: int = 3
 EARLY_STOPPING_METRIC: str = "val_loss"
 CHECKPOINT_FLAG: bool = False
 
@@ -41,17 +41,17 @@ BIAS_FLAG: bool = True
 
 # Layer stack options
 HIDDEN_DIM: int = 256
-LAYER_NORM_POSITION: LayerNormPositionOptions = LayerNormPositionOptions.DEFAULT
+LAYER_NORM_POSITION: LayerNormPositionOptions = LayerNormPositionOptions.BEFORE
 STACK_NUM_LAYERS: int = 3
-STACK_ACTIVATION: ActivationOptions = ActivationOptions.RELU
-STACK_RESIDUAL_FLAG: bool = False
-STACK_DROPOUT_PROBABILITY: float = 0.0
+STACK_ACTIVATION: ActivationOptions = ActivationOptions.GELU
+STACK_RESIDUAL_FLAG: bool = True
+STACK_DROPOUT_PROBABILITY: float = 0.2
 
 # Hyperparameter search space
 SEARCH_SPACE_LEARNING_RATE: list = [1e-4, 1e-3, 1e-2]
-SEARCH_SPACE_HIDDEN_DIM: list = [64, 128, 256, 512]
-SEARCH_SPACE_STACK_NUM_LAYERS: list = [3, 6]
-SEARCH_SPACE_STACK_DROPOUT_PROBABILITY: list = [0.0, 0.1]
+SEARCH_SPACE_HIDDEN_DIM: list = [64, 128, 256]
+SEARCH_SPACE_STACK_NUM_LAYERS: list = [3, 6, 9]
+SEARCH_SPACE_STACK_DROPOUT_PROBABILITY: list = [0.0, 0.1, 0.2]
 SEARCH_SPACE_STACK_ACTIVATION: list = [
     ActivationOptions.RELU,
     ActivationOptions.SILU,
