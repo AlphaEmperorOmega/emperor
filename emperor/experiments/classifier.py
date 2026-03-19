a4f22fabb38108038e462a7c58002c20edda1648
AlphaEmperorOmega <alpha.emperor.omega@gmail.com>
2026-03-19 14:26:06 +0100

    [CLN]: Format F1Score constructor calls in ClassifierMetricsLogger

--- emperor/experiments/classifier.py
+++ emperor/experiments/classifier.py
@@ -56,7 +56,9 @@ class ClassifierMetricsLogger(nn.Module):
         super().__init__()
         task = "multiclass"
         self.train_accuracy = torchmetrics.Accuracy(task=task, num_classes=num_classes)
-        self.train_f1_score = torchmetrics.F1Score(task=task, num_classes=num_classes, average="macro")
+        self.train_f1_score = torchmetrics.F1Score(
+            task=task, num_classes=num_classes, average="macro"
+        )
 
         self.validation_accuracy = torchmetrics.Accuracy(
             task=task, num_classes=num_classes
@@ -66,7 +68,9 @@ class ClassifierMetricsLogger(nn.Module):
         )
 
         self.test_accuracy = torchmetrics.Accuracy(task=task, num_classes=num_classes)
-        self.test_f1_score = torchmetrics.F1Score(task=task, num_classes=num_classes, average="macro")
+        self.test_f1_score = torchmetrics.F1Score(
+            task=task, num_classes=num_classes, average="macro"
+        )
 
     def log_training_step(
         self, log_fn: Callable, loss: Tensor, logits: Tensor, Y: Tensor
