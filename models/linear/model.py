0d5e64e3aa46141abb7082b11effadfe6b5dd3a0
AlphaEmperorOmega <alpha.emperor.omega@gmail.com>
2026-04-13 20:50:58 +0200

    [REF]: simplify linear Model init and use Layer.forward_with_state

--- models/linear/model.py
+++ models/linear/model.py
@@ -1,7 +1,7 @@
 import torch
 
 from torch import Tensor
-from emperor.base.utils import ConfigBase
+from emperor.base.layer.layer import Layer
 from emperor.base.layer import LayerStack, LayerStackConfig
 from emperor.experiments.classifier import ClassifierExperiment
 from models.linear.config import ExperimentConfig
@@ -18,16 +18,9 @@ class Model(ClassifierExperiment):
         cfg: "ModelConfig",
     ):
         super().__init__(cfg)
-        self.main_cfg: ExperimentConfig = self._resolve_main_config(self.cfg, cfg)
-        self.model_config: LayerStackConfig = self.main_cfg.model_config
-        self.model = LayerStack(self.model_config).build_model()
-
-    def _resolve_main_config(
-        self, sub_config: "ConfigBase", main_cfg: "ConfigBase"
-    ) -> None:
-        if sub_config.override_config is not None:
-            return sub_config.override_config
-        return main_cfg
+        self.experiment_config: ExperimentConfig = self.cfg.experiment_config
+        self.model_config: LayerStackConfig = self.experiment_config.model_config
+        self.model = LayerStack(self.model_config).build()
 
     def forward(
         self,
@@ -35,5 +28,5 @@ class Model(ClassifierExperiment):
     ) -> Tensor:
         X = X.to(self.device)
         X = torch.flatten(X, start_dim=1)
-        X = self.model(X)
+        X = Layer.forward_with_state(self.model, X)
         return X
