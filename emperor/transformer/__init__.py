ed0612c2c2bfd163ba8f4db936dd2941c264e841
AlphaEmperorOmega <alpha.emperor.omega@gmail.com>
2026-07-10 20:17:21 +0200

    [FEA]: expose transformer construction APIs

--- emperor/transformer/__init__.py
+++ emperor/transformer/__init__.py
@@ -1,25 +1,46 @@
 from emperor.transformer.config import TransformerConfig
-from emperor.transformer.model import Transformer
 from emperor.transformer.core.config import (
-    TransformerEncoderStackConfig,
-    TransformerDecoderStackConfig,
-    TransformerEncoderLayerConfig,
-    TransformerEncoderBlockLayerConfig,
+    TransformerDecoderBlockLayerConfig,
     TransformerDecoderLayerConfig,
-)
-from emperor.transformer.core.stack import (
-    TransformerEncoderStack,
-    TransformerDecoderStack,
+    TransformerDecoderStackConfig,
+    TransformerEncoderBlockLayerConfig,
+    TransformerEncoderLayerConfig,
+    TransformerEncoderStackConfig,
 )
 from emperor.transformer.core.layers import (
-    TransformerEncoderLayer,
-    TransformerEncoderBlockLayer,
+    TransformerDecoderBlockLayer,
     TransformerDecoderLayer,
+    TransformerDecoderLayerState,
+    TransformerEncoderBlockLayer,
+    TransformerEncoderLayer,
+)
+from emperor.transformer.core.stack import (
+    TransformerDecoderStack,
+    TransformerEncoderStack,
 )
 from emperor.transformer.feed_forward import (
     FeedForward,
     FeedForwardConfig,
 )
+from emperor.transformer.model import Transformer
+from emperor.transformer.options import (
+    ControllerStackOptions,
+    DynamicMemoryOptions,
+    LayerControllerOptions,
+    RecurrentControllerOptions,
+    SubmoduleStackOptions,
+    SubmoduleStackSource,
+    TransformerAttentionOptions,
+    TransformerFeedForwardOptions,
+    TransformerPathOptions,
+    TransformerStackOptions,
+    attention_options_from_config,
+    expand_transformer_path_locks,
+    feed_forward_options_from_config,
+    resolve_controller_stack_options,
+    resolve_transformer_path_options,
+)
+from emperor.transformer.submodules import configure_transformer_submodule
 
 __all__ = [
     "Transformer",
@@ -30,10 +51,29 @@ __all__ = [
     "TransformerDecoderStackConfig",
     "TransformerEncoderLayer",
     "TransformerEncoderBlockLayer",
+    "TransformerDecoderBlockLayer",
+    "TransformerDecoderLayerState",
     "TransformerDecoderLayer",
     "TransformerEncoderLayerConfig",
     "TransformerEncoderBlockLayerConfig",
+    "TransformerDecoderBlockLayerConfig",
     "TransformerDecoderLayerConfig",
     "FeedForward",
     "FeedForwardConfig",
+    "ControllerStackOptions",
+    "DynamicMemoryOptions",
+    "LayerControllerOptions",
+    "RecurrentControllerOptions",
+    "SubmoduleStackOptions",
+    "SubmoduleStackSource",
+    "TransformerAttentionOptions",
+    "TransformerFeedForwardOptions",
+    "TransformerPathOptions",
+    "TransformerStackOptions",
+    "attention_options_from_config",
+    "configure_transformer_submodule",
+    "expand_transformer_path_locks",
+    "feed_forward_options_from_config",
+    "resolve_controller_stack_options",
+    "resolve_transformer_path_options",
 ]
