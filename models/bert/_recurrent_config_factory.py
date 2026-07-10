c5c91f9688aa2f5094d85f3ea3f778129b7616d4
AlphaEmperorOmega <alpha.emperor.omega@gmail.com>
2026-07-10 19:34:08 +0200

    [REF]: remove shared BERT package infrastructure

--- models/bert/_recurrent_config_factory.py
+++
@@ -1,5 +0,0 @@
-from models.experts._recurrent_config_factory import ExpertsRecurrentConfigFactory
-
-BertRecurrentConfigFactory = ExpertsRecurrentConfigFactory
-
-__all__ = ["BertRecurrentConfigFactory"]
