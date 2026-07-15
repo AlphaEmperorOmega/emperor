41518e3945ba1e9d4eae48d9ed0a397408cfb719
AlphaEmperorOmega <alpha.emperor.omega@gmail.com>
2026-07-15 20:28:20 +0200

    [FEA]: expose attention contracts at package root

--- emperor/attention/__init__.py
+++ emperor/attention/__init__.py
@@ -1,6 +1,7 @@
 from emperor.attention.core.config import MultiHeadAttentionConfig
 from emperor.attention.core.layers import MultiHeadAttentionAbstract
 from emperor.attention.core.monitor import AttentionMonitorCallback
+from emperor.attention.core.runtime import AttentionRuntimeShape
 from emperor.attention.core.state import AttentionLayerState
 from emperor.attention.core.variants import (
     IndependentAttention,
@@ -9,16 +10,19 @@ from emperor.attention.core.variants import (
     MixtureOfAttentionHeadsConfig,
     SelfAttention,
     SelfAttentionConfig,
+    SelfAttentionProjectionStrategy,
 )
 
 __all__ = [
     "MultiHeadAttentionConfig",
     "SelfAttentionConfig",
+    "SelfAttentionProjectionStrategy",
     "IndependentAttentionConfig",
     "MixtureOfAttentionHeadsConfig",
     "MultiHeadAttentionAbstract",
     "AttentionMonitorCallback",
     "AttentionLayerState",
+    "AttentionRuntimeShape",
     "SelfAttention",
     "IndependentAttention",
     "MixtureOfAttentionHeads",
