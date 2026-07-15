b744288831e8980a8dabd42bd9c1a2ebc7a8ef2e
AlphaEmperorOmega <alpha.emperor.omega@gmail.com>
2026-07-15 18:14:17 +0200

    [STY]: format self attention layer

--- emperor/attention/core/variants/self_attention/layer.py
+++ emperor/attention/core/variants/self_attention/layer.py
@@ -1,14 +1,14 @@
-from emperor.attention.core.layers import MultiHeadAttentionAbstract
-from emperor.attention.core.handlers.reshaper import AttentionReshaper
 from emperor.attention.core.handlers.mask import Mask
-from emperor.attention.core.variants.self_attention.validator import (
-    SelfAttentionValidator,
+from emperor.attention.core.handlers.reshaper import AttentionReshaper
+from emperor.attention.core.layers import MultiHeadAttentionAbstract
+from emperor.attention.core.variants.self_attention.processor import (
+    SelfAttentionProcessor,
 )
 from emperor.attention.core.variants.self_attention.projector import (
     SelfAttentionProjector,
 )
-from emperor.attention.core.variants.self_attention.processor import (
-    SelfAttentionProcessor,
+from emperor.attention.core.variants.self_attention.validator import (
+    SelfAttentionValidator,
 )
 
 
@@ -18,7 +18,5 @@ class SelfAttention(MultiHeadAttentionAbstract):
     def _build_attention_components(self) -> None:
         self.projector = SelfAttentionProjector(self.cfg)
         self.reshaper = AttentionReshaper(self.cfg)
-        self.processor = SelfAttentionProcessor(
-            self.cfg, self.projector, self.reshaper
-        )
+        self.processor = SelfAttentionProcessor(self.cfg, self.projector, self.reshaper)
         self.masks = Mask(self.cfg)
