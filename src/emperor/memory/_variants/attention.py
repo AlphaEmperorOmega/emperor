41a35161577c6a73c79cb4880191fec820feabc5
AlphaEmperorOmega <alpha.emperor.omega@gmail.com>
2026-07-18 18:41:08 +0200

    [REF]: make attention memory data flow explicit

--- src/emperor/memory/_variants/attention.py
+++ src/emperor/memory/_variants/attention.py
@@ -52,31 +52,40 @@ class AttentionDynamicMemory(DynamicMemoryAbstract):
 
     def forward(self, logits: Tensor) -> Tensor:
         self.VALIDATOR.validate_forward_inputs(logits, self.memory_dim)
-        if self.test_time_training_flag:
-            memory_bank_flat = self._adapt_and_retrieve(
-                logits, self.memory_model, self.memory_decoder
-            )
-        else:
-            memory_bank_flat = self._run_model(self.memory_model, logits)
+        memory_bank_flat = self.__retrieve_memory_bank(logits)
         memory_bank = self.__reshape_memory_bank(memory_bank_flat)
         attended_memory = self.__attend_to_memory_bank(logits, memory_bank)
         memory_update = self._run_model(self.output_model, attended_memory)
         memory_gate = self.__compute_memory_gate(logits, memory_update)
-        return logits + memory_gate * memory_update
+        gated_memory_update = memory_gate * memory_update
+        updated_logits = logits + gated_memory_update
+        return updated_logits
+
+    def __retrieve_memory_bank(self, logits: Tensor) -> Tensor:
+        if self.test_time_training_flag:
+            return self._adapt_and_retrieve(
+                logits, self.memory_model, self.memory_decoder
+            )
+        return self._run_model(self.memory_model, logits)
 
     def __reshape_memory_bank(self, memory_bank_flat: Tensor) -> Tensor:
-        return memory_bank_flat.reshape(
-            *memory_bank_flat.shape[:-1], self.num_memory_slots, self.memory_dim
+        logit_positions_shape = memory_bank_flat.shape[:-1]
+        memory_bank = memory_bank_flat.reshape(
+            *logit_positions_shape, self.num_memory_slots, self.memory_dim
         )
+        return memory_bank
 
     def __attend_to_memory_bank(self, logits: Tensor, memory_bank: Tensor) -> Tensor:
         query = self._run_model(self.query_model, logits).unsqueeze(-2)
         keys = self._run_model(self.key_model, memory_bank)
         values = self._run_model(self.value_model, memory_bank)
         attention_scores = torch.matmul(query, keys.transpose(-2, -1))
-        attention_scores = attention_scores / (self.memory_dim**0.5)
-        attention_weights = torch.softmax(attention_scores, dim=-1)
-        return torch.matmul(attention_weights, values).squeeze(-2)
+        attention_score_scale = self.memory_dim**0.5
+        scaled_attention_scores = attention_scores / attention_score_scale
+        attention_weights = torch.softmax(scaled_attention_scores, dim=-1)
+        attended_memory = torch.matmul(attention_weights, values)
+        singleton_query_dimension = -2
+        return attended_memory.squeeze(singleton_query_dimension)
 
     def __compute_memory_gate(
         self,
