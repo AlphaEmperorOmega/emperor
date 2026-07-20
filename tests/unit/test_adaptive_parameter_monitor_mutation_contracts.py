4b721b5cc21fde8dc5f042ae352cee0994be472e
AlphaEmperorOmega <alpha.emperor.omega@gmail.com>
2026-07-20 17:20:43 +0200

    [TST]: represent disabled adaptive monitor residual as null

--- tests/unit/test_adaptive_parameter_monitor_mutation_contracts.py
+++ tests/unit/test_adaptive_parameter_monitor_mutation_contracts.py
@@ -30,7 +30,7 @@ from emperor.augmentations.adaptive_parameters._monitoring.adaptive_parameters i
     _AdaptiveParameterObservation,
     _AdaptiveParameterTrackingContext,
 )
-from emperor.augmentations.adaptive_parameters._weights.variants.soft_weighted_bank import (
+from emperor.augmentations.adaptive_parameters._weights.variants.soft_weighted_bank import (  # noqa: E501
     SoftWeightedBankDynamicWeight,
 )
 from emperor.layers import (
@@ -39,7 +39,6 @@ from emperor.layers import (
     LayerConfig,
     LayerNormPositionOptions,
     LayerStackConfig,
-    ResidualConnectionOptions,
 )
 from emperor.linears import LinearLayerConfig
 
@@ -90,7 +89,7 @@ def generator_config(
             input_dim=input_dim,
             output_dim=output_dim,
             activation=ActivationOptions.DISABLED,
-            residual_connection_option=ResidualConnectionOptions.DISABLED,
+            residual_config=None,
             dropout_probability=0.0,
             layer_norm_position=LayerNormPositionOptions.DISABLED,
             gate_config=None,
