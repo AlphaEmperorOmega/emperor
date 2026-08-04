4d6373504ace04cec66648dc51c3a0377b1d34c3
AlphaEmperorOmega <alpha.emperor.omega@gmail.com>
2026-08-04 13:09:30 +0200

    [FIX]: stabilize dataset image-grid geometry

--- src/emperor/datasets/_visualization.py
+++ src/emperor/datasets/_visualization.py
@@ -2,9 +2,11 @@ def show_images(imgs, num_rows, num_cols, titles=None, scale=1.5):
     import matplotlib.pyplot as plt
 
     figsize = (num_cols * scale, num_rows * scale)
-    _, axes = plt.subplots(num_rows, num_cols, figsize=figsize)
+    _, axes = plt.subplots(num_rows, num_cols, figsize=figsize, squeeze=False)
     axes = axes.flatten()
+    rendered_count = 0
     for i, (ax, img) in enumerate(zip(axes, imgs, strict=False)):
+        rendered_count = i + 1
         try:
             img = img.detach().numpy()
         except Exception:
@@ -14,4 +16,6 @@ def show_images(imgs, num_rows, num_cols, titles=None, scale=1.5):
         ax.axes.get_yaxis().set_visible(False)
         if titles:
             ax.set_title(titles[i])
+    for ax in axes[rendered_count:]:
+        ax.set_visible(False)
     return axes
