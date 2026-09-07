07bf6df77259b0307ca4d12d29eaf32dafc6e723
AlphaEmperorOmega <alpha.emperor.omega@gmail.com>
2026-09-07 08:07:36 +0200

    [REF]: remove obsolete neuron cluster shims

--- src/emperor/neuron/_cluster/beam_routes.py
+++
@@ -1,7 +0,0 @@
-"""Compatibility imports for saved models using the former routing location."""
-
-from emperor.neuron._cluster.routing.beam import (
-    BeamRoutingDelegate,
-)
-
-__all__ = ("BeamRoutingDelegate",)
