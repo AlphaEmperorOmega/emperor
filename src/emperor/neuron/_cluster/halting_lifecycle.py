f9db2621fc2d850f7d0f005ab3e2a76acb9a4582
AlphaEmperorOmega <alpha.emperor.omega@gmail.com>
2026-09-07 08:07:36 +0200

    [REF]: remove obsolete neuron cluster shims

--- src/emperor/neuron/_cluster/halting_lifecycle.py
+++
@@ -1,7 +0,0 @@
-"""Compatibility imports for saved models using the former routing location."""
-
-from emperor.neuron._cluster.routing.halting import (
-    _NeuronHaltingLifecycle,
-)
-
-__all__ = ("_NeuronHaltingLifecycle",)
