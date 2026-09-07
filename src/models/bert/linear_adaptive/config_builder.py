5d7840e89fe13ab7bd1f799456864e927abfe230
AlphaEmperorOmega <alpha.emperor.omega@gmail.com>
2026-09-07 08:27:24 +0200

    [FEA]: expose adaptive BERT linear options

--- src/models/bert/linear_adaptive/config_builder.py
+++ src/models/bert/linear_adaptive/config_builder.py
@@ -72,6 +72,7 @@ class _BertLinearAdaptiveConfigBuilderImplementation:
         self.encoder_recurrent_controller_options = options.recurrent_controller_options
         self.adaptive_generator_stack_options = options.adaptive_generator_stack_options
         self.hidden_adaptive_weight_options = options.hidden_adaptive_weight_options
+        self.grouping_config = options.grouping_config
         self.hidden_adaptive_bias_options = options.hidden_adaptive_bias_options
         self.hidden_adaptive_diagonal_options = options.hidden_adaptive_diagonal_options
         self.hidden_adaptive_mask_options = options.hidden_adaptive_mask_options
@@ -81,6 +82,7 @@ class _BertLinearAdaptiveConfigBuilderImplementation:
         self.attention_hidden_adaptive_weight_options = (
             options.attention_hidden_adaptive_weight_options
         )
+        self.attention_grouping_config = options.attention_grouping_config
         self.attention_hidden_adaptive_bias_options = (
             options.attention_hidden_adaptive_bias_options
         )
@@ -96,6 +98,7 @@ class _BertLinearAdaptiveConfigBuilderImplementation:
         self.feed_forward_hidden_adaptive_weight_options = (
             options.feed_forward_hidden_adaptive_weight_options
         )
+        self.feed_forward_grouping_config = options.feed_forward_grouping_config
         self.feed_forward_hidden_adaptive_bias_options = (
             options.feed_forward_hidden_adaptive_bias_options
         )
@@ -127,6 +130,7 @@ class _BertLinearAdaptiveConfigBuilderImplementation:
             hidden_dim=self.hidden_dim,
             output_dim=self.output_dim,
             adaptive_generator_stack_options=self.adaptive_generator_stack_options,
+            grouping_config=self.grouping_config,
             hidden_adaptive_weight_options=self.hidden_adaptive_weight_options,
             hidden_adaptive_bias_options=self.hidden_adaptive_bias_options,
             hidden_adaptive_diagonal_options=self.hidden_adaptive_diagonal_options,
@@ -149,6 +153,9 @@ class _BertLinearAdaptiveConfigBuilderImplementation:
                 if self.attention_adaptive_generator_stack_options is not None
                 else self.adaptive_generator_stack_options
             ),
+            grouping_config=self.attention_grouping_config
+            if self.attention_grouping_config is not None
+            else self.grouping_config,
             hidden_adaptive_weight_options=(
                 self.attention_hidden_adaptive_weight_options
                 if self.attention_hidden_adaptive_weight_options is not None
@@ -187,6 +194,9 @@ class _BertLinearAdaptiveConfigBuilderImplementation:
                 if self.feed_forward_adaptive_generator_stack_options is not None
                 else self.adaptive_generator_stack_options
             ),
+            grouping_config=self.feed_forward_grouping_config
+            if self.feed_forward_grouping_config is not None
+            else self.grouping_config,
             hidden_adaptive_weight_options=(
                 self.feed_forward_hidden_adaptive_weight_options
                 if self.feed_forward_hidden_adaptive_weight_options is not None
